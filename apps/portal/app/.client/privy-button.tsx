82b0b75ca1e8442d51c5ca0d0f68aa3b27f52b2d
jonathanprozzi <jonathanprozzi@gmail.com>
2024-04-26 16:00:10 -0400

    Cleans up testing with the shad button

--- apps/portal/app/.client/privy-button.tsx
+++ apps/portal/app/.client/privy-button.tsx
@@ -1,25 +1,26 @@
-import { usePrivy } from '@privy-io/react-auth'
+import { Button } from '@intuition-ts/1ui';
+import { usePrivy } from '@privy-io/react-auth';
 
 export function PrivyButton() {
-  const { ready, authenticated, login, user: privyUser } = usePrivy()
+  const { ready, authenticated, login, user: privyUser } = usePrivy();
 
   // Disable login when Privy is not ready or the user is already authenticated
-  const disableLogin = !ready || (ready && authenticated)
+  const disableLogin = !ready || (ready && authenticated);
 
-  console.log('ready', ready)
-  console.log('authenticated', authenticated)
-  console.log('privyUser', privyUser)
+  console.log('ready', ready);
+  console.log('authenticated', authenticated);
+  console.log('privyUser', privyUser);
 
   if (!ready) {
-    return null
+    return null;
   }
 
   if (ready && !authenticated) {
     return (
-      <button disabled={disableLogin} onClick={login}>
+      <Button disabled={disableLogin} onClick={login}>
         Log in
-      </button>
-    )
+      </Button>
+    );
   }
 
   if (ready && authenticated && privyUser !== null) {
@@ -27,6 +28,6 @@ export function PrivyButton() {
       <button disabled={disableLogin} onClick={login}>
         User: {privyUser.id}
       </button>
-    )
+    );
   }
 }
--- apps/portal/app/routes/_index.tsx
+++ apps/portal/app/routes/_index.tsx
@@ -1,4 +1,3 @@
-import { Button } from '@intuition-ts/1ui';
 import { PrivyButton } from '~/.client/privy-button';
 import { ThemeTest } from '~/components/theme-test';
 
@@ -7,7 +6,6 @@ export default function Index() {
     <div>
       <PrivyButton />
       <ThemeTest />
-      <Button variant="secondary">Secondary</Button>
     </div>
   );
 }
