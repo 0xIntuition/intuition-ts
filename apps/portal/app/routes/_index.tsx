18894170fa0a450982407cef47e20a1007f6cfbb
jonathanprozzi <jonathanprozzi@gmail.com>
2024-04-29 14:19:21 -0400

    Resolves type issues, back to stable

--- apps/portal/app/routes/_index.tsx
+++ apps/portal/app/routes/_index.tsx
@@ -1,14 +1,13 @@
-import { Button, Input, Label } from '@intuition-ts/1ui';
-import { ThemeTest } from '~/components/theme-test';
+import { Button, Label, Input } from '@intuition-ts/1ui';
 
 export default function Index() {
   return (
     <div className="flex flex-col items-center gap-4 bg-primary-900 m-8">
       <div className="flex flex-col">
-        <Button variant="default">Test 1ui Button</Button>
-        <Button variant="secondary">Test 1ui Button</Button>
-        <Button variant="outline">Test 1ui Button</Button>
-        <Button variant="link">Test 1ui Button</Button>
+        <Button variant="default">Default Button</Button>
+        <Button variant="secondary">Secondary Button</Button>
+        <Button variant="outline">Outline Button</Button>
+        <Button variant="destructive">Destructive Button</Button>
       </div>
       <Label htmlFor="textTest">Test Text Input</Label>
       <Input type="text" id="fileTest" placeholder="Test text input" />
