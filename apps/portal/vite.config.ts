96e1040fb65504a485bcfd80b03745b39a889e1d
Rahul <37151383+rahulr8@users.noreply.github.com>
2024-05-08 08:11:58 -0700

    path fixes

--- apps/portal/vite.config.ts
+++ apps/portal/vite.config.ts
@@ -3,10 +3,22 @@ import { installGlobals } from '@remix-run/node'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { flatRoutes } from 'remix-flat-routes'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+import postcssImport from 'postcss-import'
 
 installGlobals()
 
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [
+        postcssImport, // Helps handle imports in CSS if needed
+        tailwindcss(), // Path to your Tailwind config if it's not in the root
+        autoprefixer(),
+      ],
+    },
+  },
   plugins: [
     remix({
       ignoredRouteFiles: ['**/.*'],
