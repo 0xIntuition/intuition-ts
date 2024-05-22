856861e7b67e8fe589421d53bcbff77ea406b94a
alexander-mann <alex.mann@noviconnect.com>
2024-05-22 11:49:06 -0400

    feat: finalize icons

--- packages/1ui/src/components/Icon/Icon.stories.tsx
+++ packages/1ui/src/components/Icon/Icon.stories.tsx
@@ -1,6 +1,84 @@
 import React from 'react'
 import type { Meta, StoryObj } from '@storybook/react'
-import { Icon } from './Icon'
+import { Icon, IconName } from './Icon'
+
+const iconOptions = [
+  'arrow-left',
+  'arrow-right',
+  'arrow-up-right',
+  'square-arrow-top-right',
+  'rescue-ring',
+  'bubble-annotation',
+  'megaphone',
+  'graduate-cap',
+  'play-circle',
+  'play',
+  'book',
+  'bookmark-check',
+  'shield-check',
+  'square-check',
+  'square-check-empty',
+  'folder',
+  'circle-question-mark',
+  'circle-arrow',
+  'trash-can',
+  'settings-gear',
+  'floppy-disk-1',
+  'floppy-disk-2',
+  'medal',
+  'wreath',
+  'chevron-large-down',
+  'chevron-bottom',
+  'chevron-right',
+  'chevron-left',
+  'sparkle',
+  'arrow-box-left',
+  'lock',
+  'magnifying-glass',
+  'filter-1',
+  'fast-forward',
+  'crystal-ball',
+  'circle-dots-center',
+  'arrow-out-of-box',
+  'cross-large',
+  'circle-x',
+  'square-x',
+  'moneybag',
+  'rocket',
+  'brush-sparkle',
+  'fork-knife',
+  'shopping-bag',
+  'layout-third',
+  'layout-grid',
+  'filter-2',
+  'chevron-double-left',
+  'chevron-double-right',
+  'chevron-left-small',
+  'chevron-right-small',
+  'arrows-repeat',
+  'loader',
+  'group',
+  'wallet',
+  'chevron-top-small',
+  'chevron-down-small',
+  'chevron-grabber-vertical',
+  'circle-check',
+  'crypto-punk',
+  'person-circle',
+  'shield-check',
+  'plus-large',
+  'plus-small',
+  'circle-plus',
+  'square-plus',
+  'eye-open',
+  'eye-slash',
+  'calendar',
+  'shield-check-blue',
+  'dot-grid',
+  'circles-three',
+  'circle',
+  'microphone',
+]
 
 const meta: Meta<typeof Icon> = {
   title: 'Components/Icon',
@@ -16,30 +94,7 @@ const meta: Meta<typeof Icon> = {
   argTypes: {
     name: {
       description: 'Icon name',
-      options: [
-        'arrow-left',
-        'arrow-right',
-        'arrow-up-right',
-        'square-arrow-top-right',
-        'rescue-ring',
-        'bubble-annotation',
-        'megaphone',
-        'graduate-cap',
-        'play-circle',
-        'play',
-        'book',
-        'bookmark-check',
-        'shield-check',
-        'square-check',
-        'square-check-empty',
-        'folder',
-        'circle-question-mark',
-        'circle-arrow',
-        'trash-can',
-        'settings-gear',
-        'floppy-disk-1',
-        'floppy-disk-2',
-      ],
+      options: iconOptions,
       table: {
         type: { summary: 'string' },
       },
@@ -62,42 +117,20 @@ type Story = StoryObj<typeof Icon>
 export const BasicUsage: Story = {
   render: (args) => <Icon {...args} />,
   args: {
-    name: 'arrow-left',
-    className: 'text-accent h-9 w-9',
+    name: 'crystal-ball',
+    className: 'h-10 w-10',
   },
 }
 
 export const AllIcons: Story = {
   render: (args) => (
     <div
-      style={{ display: 'flex', gap: '24px', flexWrap: 'wrap', width: '505px' }}
+      style={{ display: 'flex', gap: '24px', flexWrap: 'wrap', width: '700px' }}
       {...args}
     >
-      <Icon name="arrow-left" />
-      <Icon name="arrow-right" />
-      <Icon name="arrow-up-right" />
-      <Icon name="square-arrow-top-right" />
-      <Icon name="rescue-ring" />
-      <Icon name="bubble-annotation" />
-      <Icon name="megaphone" />
-      <Icon name="graduate-cap" />
-      <Icon name="play-circle" />
-      <Icon name="play" />
-      <Icon name="book" />
-      <Icon name="bookmark-check" />
-      <Icon name="shield-check" />
-      <Icon name="square-check" />
-      <Icon name="square-check-empty" />
-      <Icon name="folder" />
-      <Icon name="circle-question-mark" />
-      <Icon name="circle-arrow" />
-      <Icon name="trash-can" />
-      <Icon name="settings-gear" />
-      <Icon name="floppy-disk-1" />
-      <Icon name="floppy-disk-2" />
-      <Icon name="" />
-      <Icon name="" />
-      <Icon name="" />
+      {iconOptions.map((iconName, index) => (
+        <Icon key={index} name={iconName as IconName} />
+      ))}
     </div>
   ),
 }
