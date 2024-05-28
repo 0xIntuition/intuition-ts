6cb979217095d27a28e63a9f17a25e4b76426976
alexander-mann <alex.mann@noviconnect.com>
2024-05-28 12:00:40 -0400

    feat: add test

--- packages/1ui/src/components/Popover/Popover.spec.tsx
+++ packages/1ui/src/components/Popover/Popover.spec.tsx
@@ -2,18 +2,39 @@ import React from 'react'
 
 import { render } from '@testing-library/react'
 
-import { Popover } from './Popover'
+import { Button, Popover, PopoverContent, PopoverTrigger, Text } from '..'
 
 describe('Popover', () => {
-  // Example assertion: Check if the component renders a specific text
-  it('should render appropriate element', () => {
-    const { asFragment } = render(<Popover>Something</Popover>)
+  it('should render appropriate elements', () => {
+    const { asFragment } = render(
+      <Popover>
+        <PopoverTrigger>
+          <Button size="lg">Open popover</Button>
+        </PopoverTrigger>
+        <PopoverContent>
+          <Text variant="bodyLarge">Popover Content</Text>
+          <Text variant="body">
+            Here is some very long content to test out this popover. It is good
+            to see how it looks with lots of text!
+          </Text>
+        </PopoverContent>
+      </Popover>,
+    )
     expect(asFragment()).toMatchInlineSnapshot(`
-    <DocumentFragment>
-      <p>Something</p>>
-    </DocumentFragment>
+      <DocumentFragment>
+        <button
+          aria-controls="radix-:r0:"
+          aria-expanded="false"
+          aria-haspopup="dialog"
+          data-state="closed"
+          type="button"
+        />
+        <button
+          class="flex items-center font-medium border-solid border disabled:bg-muted disabled:text-muted-foreground disabled:border-muted shadow-md-subtle bg-primary text-primary-foreground border-primary hover:bg-primary/80 rounded-full px-4 py-2 gap-4 text-base"
+        >
+          Open popover
+        </button>
+      </DocumentFragment>
     `)
   })
-  // Add more tests as needed to cover the functionality of your component
-  // Additional tests can be written here to check different states and props
 })
