501ba31155f6f3decb603920cf33df7744497375
alexander-mann <alex.mann@noviconnect.com>
2024-05-28 11:48:36 -0400

    feat: add argtypes

--- packages/1ui/src/components/Popover/Popover.stories.tsx
+++ packages/1ui/src/components/Popover/Popover.stories.tsx
@@ -7,6 +7,26 @@ import { Button, Popover, PopoverContent, PopoverTrigger, Text } from '..'
 const meta: Meta<typeof Popover> = {
   title: 'Components/Popover',
   component: Popover,
+  argTypes: {
+    side: {
+      describe: 'Side the content will open on',
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: 'bottom' },
+      },
+      options: ['top', 'right', 'bottom', 'left'],
+      control: 'select',
+    },
+    align: {
+      describe: 'Where to align content to trigger',
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: 'center' },
+      },
+      options: ['start', 'center', 'end'],
+      control: 'select',
+    },
+  },
 }
 
 export default meta
@@ -14,13 +34,12 @@ export default meta
 type Story = StoryObj<typeof Popover>
 
 export const BasicUsage: Story = {
-  args: {},
   render: (args) => (
-    <Popover {...args}>
+    <Popover>
       <PopoverTrigger>
         <Button size="lg">Open popover</Button>
       </PopoverTrigger>
-      <PopoverContent>
+      <PopoverContent {...args}>
         <Text variant="bodyLarge">Popover Content</Text>
         <Text variant="body">
           Here is some very long content to test out this popover. It is good to
