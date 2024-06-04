cc665819eee0de40738ec86c60eccb783a5cad06
Simonas Karuzas <simonas@not.cat>
2024-06-04 23:46:39 +0300

    feat(substream): simulate spam from random users

--- packages/substream/simulation/unknown.ts
+++ packages/substream/simulation/unknown.ts
@@ -3,12 +3,14 @@ import { getIntuition } from './lib/utils'
 import { faker } from '@faker-js/faker'
 
 async function main() {
-  const count = 500
+  const count = 400
 
-  const user = await getIntuition(0)
-  const costWithDeposit = await user.multivault.getAtomCost()
+  const costWithDeposit = await (await getIntuition(0)).multivault.getAtomCost()
   for (let i = 1; i < count; i++) {
     const uri = faker.lorem.words(3)
+
+    const rnd = Math.floor(Math.random() * 100)
+    const user = await getIntuition(rnd)
 
     const hash = await user.multivault.contract.write.createAtom([toHex(uri)], {
       value: costWithDeposit,
