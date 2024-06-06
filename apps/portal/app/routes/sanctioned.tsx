5166800ba05098b87f9ba19f1e589aebd6721154
jonathanprozzi <jonathanprozzi@gmail.com>
2024-06-06 15:22:23 -0400

    Removes duplicate viem.ts from /lib/utils. Adds initial layout and copy to /sanctioned route

--- apps/portal/app/routes/sanctioned.tsx
+++ apps/portal/app/routes/sanctioned.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@0xintuition/1ui'
+
 import { chainalysisOracleAbi } from '@lib/abis/chainalysisOracle'
 import { SessionContext } from '@middleware/session'
 import { LoaderFunctionArgs, redirect } from '@remix-run/node'
 import { mainnetClient } from '@server/viem'
+import { SquareX } from 'lucide-react'
 
 export async function loader({ context }: LoaderFunctionArgs) {
   const session = context.get(SessionContext)
@@ -23,9 +26,27 @@ export async function loader({ context }: LoaderFunctionArgs) {
 
 export default function Sanctioned() {
   return (
-    <div className="mt-16 flex h-full w-full flex-col items-center gap-8">
-      <div className="flex w-[92vw] max-w-[728px] flex-col items-center justify-center gap-8 p-16 text-center">
-        Sanctioned!
+    <div className="mt-16 flex h-full w-full flex-col items-center">
+      <div className="flex w-[92vw] max-w-[728px] flex-col p-16 max-w-xl gap-2.5">
+        <div className="flex items-center text-secondary-foreground/70 gap-2">
+          <SquareX />
+          <span>Access Restricted</span>
+        </div>
+        <h2 className="text-white text-semibold text-3xl lg:text-4xl">
+          Wallet Address Sanctioned
+        </h2>
+        <p className="text-secondary-foreground/40 text-lg font-medium leading-6">
+          We&apos;re sorry, but access to Intuition is restricted due to your
+          wallet address being associated with a sanctioned entity. Please
+          contact our support team for more information.
+        </p>
+        <p className="text-secondary-foreground/40 text-lg font-medium leading-6">
+          In the meantime, join our Discord community to stay updated and
+          connect with us!
+        </p>
+        <Button className="w-fit mt-3.5" variant="secondary">
+          Join our Discord
+        </Button>
       </div>
     </div>
   )
