297082e179952000337bc98aeca1f525b873c1fb
Simonas Karuzas <simonas@not.cat>
2024-06-08 02:10:44 +0300

    feat(explorer): triple feed

--- apps/explorer/src/Feed.tsx
+++ apps/explorer/src/Feed.tsx
@@ -1,23 +1,29 @@
 import { useQuery, gql } from '@apollo/client';
 import {
-  Label, Avatar, AvatarFallback, AvatarImage
+  Button,
+  Label, Avatar, AvatarFallback, AvatarImage, HoverCard, HoverCardTrigger
 } from '@0xintuition/1ui'
+import { AtomHoverCardContent } from './AtomHoverCardContent';
 
 const GET_FEED = gql`
 query Feed {
   triples {
     id
     subject {
+      id
       emoji
       label
       image
     }
     predicate {
+      id
       emoji
       label
     }
     object {
+      id
       emoji
+      image
       label
     }
   }
@@ -29,24 +35,38 @@ export function Feed() {
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error : {error.message}</p>;
-
   return (<ul className="list-none list-inside w-full">
     {data.triples.map((triple: any) => (
       <li key={triple.id} className="p-3 border-border/30 border-b-[1px] border-solid">
         <div className="flex items-center space-x-4">
-          <Avatar >
-            <AvatarImage src={triple.subject.image} alt="intuition" />
-            <AvatarFallback>{triple.subject.emoji}</AvatarFallback>
-          </Avatar>
-          <Label>{triple.subject.label}</Label>
+
+          <HoverCard>
+            <AtomHoverCardTrigger atom={triple.subject} />
+            <AtomHoverCardContent atomId={triple.subject.id} />
+          </HoverCard>
+
           <Label>{triple.predicate.emoji} {triple.predicate.label}</Label>
-          <Avatar>
-            <AvatarImage src={triple.object.image} alt="intuition" />
-            <AvatarFallback>{triple.object.emoji}</AvatarFallback>
-          </Avatar>
-          <Label>{triple.object.label}</Label>
+
+          <HoverCard>
+            <AtomHoverCardTrigger atom={triple.object} />
+            <AtomHoverCardContent atomId={triple.object.id} />
+          </HoverCard>
         </div>
       </li>
     ))}
   </ul>)
+}
+
+function AtomHoverCardTrigger({ atom }: { atom: any }) {
+  return (
+    <HoverCardTrigger asChild>
+      <Button variant='text'>
+        <Avatar >
+          <AvatarImage src={atom.image} alt="intuition" />
+          <AvatarFallback>{atom.emoji}</AvatarFallback>
+        </Avatar>
+        <Label>{atom.label}</Label>
+      </Button>
+    </HoverCardTrigger>
+  )
 }
