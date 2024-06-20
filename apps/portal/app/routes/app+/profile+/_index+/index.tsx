a09e1b93a7e58ae3bdfa7636e30347a2293bb6c7
jonathanprozzi <jonathanprozzi@gmail.com>
2024-06-20 13:23:35 -0400

    Adds a placeholder quest route and adds in the QuestHeaderCard to the user profile

--- apps/portal/app/routes/app+/profile+/_index+/index.tsx
+++ apps/portal/app/routes/app+/profile+/_index+/index.tsx
@@ -1,6 +1,8 @@
+import { QuestHeaderCard } from '@0xintuition/1ui'
+
 import logger from '@lib/utils/logger'
 import { json } from '@remix-run/node'
-import { useLoaderData } from '@remix-run/react'
+import { useLoaderData, useNavigate } from '@remix-run/react'
 
 export async function loader() {
   return json({
@@ -10,11 +12,20 @@ export async function loader() {
 
 export default function ProfileOverview() {
   const { message } = useLoaderData<typeof loader>()
+  const navigate = useNavigate()
   logger('message from profile overview loader', message)
 
   return (
-    <div className="flex flex-col items-center gap-4">
-      this is the profile overview
+    <div className="flex flex-col gap-10 mt-10">
+      <QuestHeaderCard
+        title="Primitive Island"
+        subtitle="Continue your journey."
+        numberOfCompletedQuests={1}
+        totalNumberOfQuests={10}
+        onButtonClick={() => navigate('/app/quests')} // we can change this as we lock in the quests route struture
+      />
+      <h2>About</h2>
+      <h2>User Stats</h2>
     </div>
   )
 }
