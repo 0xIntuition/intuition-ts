709c1344d9aa8a5a969fbbc407d6afd609fc501f
jonathanprozzi <jonathanprozzi@gmail.com>
2024-06-20 13:41:06 -0400

    Adds mock loader data to the profile for the quests, adds About and User Stats section titles to profile

--- apps/portal/app/routes/app+/profile+/_index+/index.tsx
+++ apps/portal/app/routes/app+/profile+/_index+/index.tsx
@@ -1,31 +1,40 @@
 import { QuestHeaderCard } from '@0xintuition/1ui'
 
-import logger from '@lib/utils/logger'
 import { json } from '@remix-run/node'
 import { useLoaderData, useNavigate } from '@remix-run/react'
 
 export async function loader() {
+  const mockUserQuestsData = {
+    currentQuest: {
+      title: 'Primitive Island',
+      subtitle: 'Continue your journey',
+    },
+    questsCompleted: 1,
+    totalQuests: 10,
+  }
+
   return json({
-    message: 'hack the planet',
+    userQuests: mockUserQuestsData,
   })
 }
 
 export default function ProfileOverview() {
-  const { message } = useLoaderData<typeof loader>()
+  const { userQuests } = useLoaderData<typeof loader>()
   const navigate = useNavigate()
-  logger('message from profile overview loader', message)
 
   return (
     <div className="flex flex-col gap-10 mt-10">
       <QuestHeaderCard
-        title="Primitive Island"
-        subtitle="Continue your journey."
-        numberOfCompletedQuests={1}
-        totalNumberOfQuests={10}
-        onButtonClick={() => navigate('/app/quests')} // we can change this as we lock in the quests route struture
+        title={userQuests.currentQuest.title}
+        subtitle={userQuests.currentQuest.subtitle}
+        numberOfCompletedQuests={userQuests.questsCompleted}
+        totalNumberOfQuests={userQuests.totalQuests}
+        onButtonClick={() => navigate('/app/quests')}
       />
-      <h2>About</h2>
-      <h2>User Stats</h2>
+      <h2 className="font-medium text-xl text-secondary-foreground">About</h2>
+      <h2 className="font-medium text-xl text-secondary-foreground">
+        User Stats
+      </h2>
     </div>
   )
 }
