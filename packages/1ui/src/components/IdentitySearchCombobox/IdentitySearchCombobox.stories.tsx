51bf09c8db9e06bd3f9d70b7f132d06d025fd66e
alexander-mann <alexandercmann@gmail.com>
2024-06-28 09:49:50 -0400

    fix: it all up!

--- packages/1ui/src/components/IdentitySearchCombobox/IdentitySearchCombobox.stories.tsx
+++ packages/1ui/src/components/IdentitySearchCombobox/IdentitySearchCombobox.stories.tsx
@@ -10,6 +10,9 @@ import {
 const meta: Meta<typeof IdentitySearchCombobox> = {
   title: 'Components/IdentitySearchCombobox',
   component: IdentitySearchCombobox,
+  // eslint-disable-next-line
+  // @ts-ignore
+  subcomponents: { IdentitySearchComboboxItem },
 }
 
 export default meta
@@ -17,12 +20,81 @@ export default meta
 type Story = StoryObj<typeof IdentitySearchCombobox>
 
 export const BasicUsage: Story = {
-  args: {},
-  render: (args) => (
-    <IdentitySearchCombobox {...args}>
-      <IdentitySearchComboboxItem value="1" />
-      <IdentitySearchComboboxItem value="2" />
-      <IdentitySearchComboboxItem value="3" />
+  render: () => (
+    <IdentitySearchCombobox
+      onCreateIdentityClick={() => console.log('Button clicked!')}
+    >
+      <IdentitySearchComboboxItem
+        variant="non-user"
+        name="mech"
+        value={0.039}
+        walletAddress="0x1234567890abcdef1234567890abcdef12345678"
+        socialCount={34}
+        tagCount={34}
+      />
+      <IdentitySearchComboboxItem
+        variant="non-user"
+        name="mechanical"
+        value={0.345}
+        walletAddress="0x1234567890abcdef1234567890abcdef12345678"
+        socialCount={34}
+        tagCount={34}
+      />
+      <IdentitySearchComboboxItem
+        variant="non-user"
+        name="mechanize"
+        value={0.003}
+        walletAddress="0x1234567890abcdef1234567890abcdef12345678"
+        socialCount={34}
+        tagCount={34}
+      />
+      <IdentitySearchComboboxItem
+        variant="non-user"
+        name="mechanist"
+        value={0.019}
+        walletAddress="0x1234567890abcdef1234567890abcdef12345678"
+        socialCount={34}
+        tagCount={34}
+      />
+    </IdentitySearchCombobox>
+  ),
+}
+
+export const WithoutCreateIdentityButton: Story = {
+  render: () => (
+    <IdentitySearchCombobox>
+      <IdentitySearchComboboxItem
+        variant="non-user"
+        name="mech"
+        value={0.039}
+        walletAddress="0x1234567890abcdef1234567890abcdef12345678"
+        socialCount={34}
+        tagCount={34}
+      />
+      <IdentitySearchComboboxItem
+        variant="non-user"
+        name="mechanical"
+        value={0.345}
+        walletAddress="0x1234567890abcdef1234567890abcdef12345678"
+        socialCount={34}
+        tagCount={34}
+      />
+      <IdentitySearchComboboxItem
+        variant="non-user"
+        name="mechanize"
+        value={0.003}
+        walletAddress="0x1234567890abcdef1234567890abcdef12345678"
+        socialCount={34}
+        tagCount={34}
+      />
+      <IdentitySearchComboboxItem
+        variant="non-user"
+        name="mechanist"
+        value={0.019}
+        walletAddress="0x1234567890abcdef1234567890abcdef12345678"
+        socialCount={34}
+        tagCount={34}
+      />
     </IdentitySearchCombobox>
   ),
 }
--- packages/1ui/src/components/IdentitySearchCombobox/IdentitySearchCombobox.tsx
+++ packages/1ui/src/components/IdentitySearchCombobox/IdentitySearchCombobox.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react'
 
 import { CommandGroup } from 'cmdk'
+import { Currency, Identity } from 'types'
 
 import {
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
   Badge,
   BadgeVariant,
   Button,
@@ -16,18 +14,23 @@ import {
   CommandInput,
   CommandItem,
   CommandList,
-  CommandSeparator,
   Icon,
   IconName,
+  IdentityCard,
+  IdentityCardProps,
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
 } from '..'
 
 export interface IdentitySearchComboboxProps
   extends React.HTMLAttributes<HTMLDivElement> {
-  onCreateIdentityClick: () => void
+  onCreateIdentityClick?: () => void
 }
 
 const IdentitySearchCombobox = ({
-  onCreateIdentityClick,
+  onCreateIdentityClick = undefined,
   children,
   ...props
 }: IdentitySearchComboboxProps) => {
@@ -35,16 +38,17 @@ const IdentitySearchCombobox = ({
     <div className="min-w-96" {...props}>
       <Command>
         <CommandInput placeholder="Search for an identity..." />
-        <Button
-          variant={ButtonVariant.text}
-          size={ButtonSize.lg}
-          className="gap-1.5 font-light justify-start p-3"
-          onClick={onCreateIdentityClick}
-        >
-          <Icon name={IconName.plusLarge} className="h-4 w-4" />
-          Create a new identity
-        </Button>
-        <CommandSeparator />
+        {onCreateIdentityClick !== undefined && (
+          <Button
+            variant={ButtonVariant.text}
+            size={ButtonSize.lg}
+            className="gap-1.5 font-light justify-start p-3 border-border/30 border-0 border-b"
+            onClick={onCreateIdentityClick}
+          >
+            <Icon name={IconName.plusLarge} className="h-4 w-4" />
+            Create a new identity
+          </Button>
+        )}
         <CommandList>
           <CommandEmpty>No results found.</CommandEmpty>
           <CommandGroup>{children}</CommandGroup>
@@ -54,32 +58,62 @@ const IdentitySearchCombobox = ({
   )
 }
 
-export interface IdentitySearchComboboxItemProps {
-  value: string
+export interface IdentitySearchComboboxItemProps extends IdentityCardProps {
+  socialCount?: number
+  tagCount?: number
+  onClick?: () => void
 }
 
 const IdentitySearchComboboxItem = ({
+  variant = Identity.user,
+  avatarSrc,
+  name,
   value,
+  currency = Currency.ETH,
+  walletAddress,
+  socialCount = 0,
+  tagCount = 0,
+  onClick,
 }: IdentitySearchComboboxItemProps) => {
   return (
-    <CommandItem className="border border-transparent rounded-lg aria-selected:bg-primary/10 aria-selected:text-primary hover:border-border/30">
+    <CommandItem
+      onClick={onClick}
+      className="border border-transparent rounded-lg aria-selected:bg-primary/10 aria-selected:text-primary hover:border-border/30 px-2 py-4"
+    >
       <div className="flex justify-between items-center w-full">
-        <div className="flex items-center gap-2">
-          <Avatar>
-            <AvatarImage src="test" alt="test" />
-            <AvatarFallback>FR</AvatarFallback>
-          </Avatar>
-          <div>{value}</div>
-        </div>
+        <IdentityCard
+          variant={variant}
+          avatarSrc={avatarSrc}
+          name={name}
+          value={value}
+          currency={currency}
+          walletAddress={walletAddress}
+        />
         <div className="flex items-center gap-1">
-          <Badge variant={BadgeVariant.social}>
-            <Icon name={IconName.tag} className="h-3 w-3" />
-            32
-          </Badge>
-          <Badge variant={BadgeVariant.default}>
-            <Icon name={IconName.tag} className="h-3 w-3" />
-            32
-          </Badge>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger>
+                <Badge variant={BadgeVariant.social}>
+                  <Icon name={IconName.trustCircle} className="h-3 w-3" />
+                  {socialCount}
+                </Badge>
+              </TooltipTrigger>
+              <TooltipContent>
+                Members in this trust circle identity
+              </TooltipContent>
+            </Tooltip>
+            <Tooltip>
+              <TooltipTrigger>
+                <Badge variant={BadgeVariant.default}>
+                  <Icon name={IconName.tag} className="h-3 w-3" />
+                  {tagCount}
+                </Badge>
+              </TooltipTrigger>
+              <TooltipContent>
+                Identities tagged with this identity
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
         </div>
       </div>
     </CommandItem>
