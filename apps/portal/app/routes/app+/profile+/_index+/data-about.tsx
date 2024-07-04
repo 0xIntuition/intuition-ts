ebfb8fa72b0af7dbfa46efc543614024cd2d846b
Vitalsine85 <rob@creative-element.net>
2024-07-03 23:00:06 -0300

    add scrolling to nested layout, improve searchParams

--- apps/portal/app/routes/app+/profile+/_index+/data-about.tsx
+++ apps/portal/app/routes/app+/profile+/_index+/data-about.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import {
   IdentityPosition,
@@ -30,6 +30,7 @@ import {
   SortDirection,
 } from '@0xintuition/api'
 
+import { useLiveLoader } from '@lib/hooks/useLiveLoader'
 import logger from '@lib/utils/logger'
 import {
   calculateTotalPages,
@@ -38,7 +39,7 @@ import {
 } from '@lib/utils/misc'
 import { SessionContext } from '@middleware/session'
 import { json, LoaderFunctionArgs } from '@remix-run/node'
-import { useFetcher, useLoaderData } from '@remix-run/react'
+import { useFetcher, useSearchParams } from '@remix-run/react'
 import { getPrivyAccessToken } from '@server/privy'
 import { formatUnits } from 'viem'
 
@@ -72,10 +73,9 @@ export async function loader({ context, request }: LoaderFunctionArgs) {
 
   const url = new URL(request.url)
   const searchParams = new URLSearchParams(url.search)
-  const positionsSortBy: PositionSortColumn =
-    (searchParams.get('sortBy') as PositionSortColumn) ?? 'createdAt'
-  const positionsDirection: SortDirection =
-    (searchParams.get('direction') as SortDirection) ?? 'desc'
+  const search = searchParams.get('search')
+  const sortBy = searchParams.get('sortBy') ?? 'UpdatedAt'
+  const direction = searchParams.get('direction') ?? 'asc'
   const page = searchParams.get('page')
     ? parseInt(searchParams.get('page') as string)
     : 1
@@ -91,8 +91,8 @@ export async function loader({ context, request }: LoaderFunctionArgs) {
         offset: 0,
       },
       sort: {
-        sortBy: positionsSortBy,
-        direction: positionsDirection,
+        sortBy: sortBy as PositionSortColumn,
+        direction: direction as SortDirection,
       },
     })
   } catch (error: unknown) {
@@ -107,12 +107,19 @@ export async function loader({ context, request }: LoaderFunctionArgs) {
   const totalPages = calculateTotalPages(positions?.total ?? 0, Number(limit))
 
   console.log('positions', positions)
+
+  console.log('search', search)
+  console.log('sortBy', sortBy)
+  console.log('direction', direction)
+  console.log('page', page)
+  console.log('limit', limit)
+
   return json({
     userIdentity,
     positions,
-    positionsSortBy,
-    positionsDirection,
-    positionsPagination: {
+    sortBy,
+    direction,
+    pagination: {
       page: Number(page),
       limit: Number(limit),
       total: positions?.total,
@@ -123,53 +130,63 @@ export async function loader({ context, request }: LoaderFunctionArgs) {
 
 export default function ProfileDataAbout() {
   return (
-    <div className="flex-col justify-start items-start gap-6 flex w-full">
+    <div className="flex-col justify-start items-start flex w-full">
       <PositionsOnIdentity />
     </div>
   )
 }
 
 export function PositionsOnIdentity() {
-  const initialData = useLoaderData<typeof loader>()
-  const { userIdentity } = initialData
+  const initialData = useLiveLoader<typeof loader>(['attest'])
+  const { userIdentity, pagination } = initialData
   const fetcher = useFetcher<typeof loader>()
-  const page = useRef(1)
   const [positions, setPositions] = useState<PositionPresenter[]>(
     initialData.positions?.data ?? [],
   )
-  const [sortBy, setSortBy] = useState(initialData.positionsSortBy)
-  const [direction, setDirection] = useState(initialData.positionsDirection)
+  const [searchParams, setSearchParams] = useSearchParams()
 
+  console.log('fetcher.data', fetcher.data)
   useEffect(() => {
     if (fetcher.data) {
-      setPositions(fetcher.data.positions.data as PositionPresenter[])
+      setPositions(fetcher.data.positions?.data as PositionPresenter[])
     }
   }, [fetcher.data])
 
+  useEffect(() => {
+    setPositions(initialData.positions?.data as PositionPresenter[])
+  }, [initialData.positions])
+
   const options = [
-    { value: 'Updated At', sortBy: 'updated_at' },
-    { value: 'Total Conviction', sortBy: 'conviction' },
-    { value: 'Total ETH', sortBy: 'assets' },
+    { value: 'Updated At', sortBy: 'UpdatedAt' },
+    { value: 'Total ETH', sortBy: 'Assets' },
   ]
 
   const handleSortChange = (
-    newSortBy: string,
-    newDirection?: 'asc' | 'desc',
+    newSortBy: PositionSortColumn,
+    newDirection: SortDirection,
   ) => {
-    if (newDirection) {
-      setDirection(newDirection)
-    } else if (sortBy !== newSortBy) {
-      setDirection('desc')
-    }
-    setSortBy(newSortBy)
-    fetcher.load(
-      `?index&page=${page.current}&sortBy=${newSortBy}&direction=${newDirection || direction}`,
-    )
+    setSearchParams({
+      ...Object.fromEntries(searchParams),
+      sortBy: newSortBy,
+      direction: newDirection,
+      page: '1',
+    })
+  }
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newSearchValue = event.target.value
+    setSearchParams({
+      ...Object.fromEntries(searchParams),
+      search: newSearchValue,
+      page: '1',
+    })
   }
 
   const onPageChange = (newPage: number) => {
-    const params = `?index&page=${newPage}&sortBy=${sortBy}&direction=${direction}`
-    fetcher.load(params)
+    setSearchParams({
+      ...Object.fromEntries(searchParams),
+      page: newPage.toString(),
+    })
   }
 
   return (
@@ -219,8 +236,8 @@ export function PositionsOnIdentity() {
           </div>
         </div>
       </div>
-      <div className="flex flex-row justify-between w-full">
-        <Input className="w-[196px]" />
+      <div className="flex flex-row justify-between w-full mt-6">
+        <Input className="w-[196px]" onChange={handleSearchChange} />
         <Select
           onValueChange={(value) => {
             const selectedOption = options.find(
@@ -246,77 +263,66 @@ export function PositionsOnIdentity() {
           </SelectContent>
         </Select>
       </div>
-      {positions?.map((position) => (
-        <div
-          key={position.id}
-          className={`grow shrink basis-0 self-stretch p-6 bg-black first:rounded-t-xl last:rounded-b-xl border border-neutral-300/20 flex-col justify-start items-start gap-5 inline-flex ${positions.length === 1 && 'rounded-xl'}`}
-        >
-          <IdentityPosition
-            variant="user"
-            avatarSrc={position.user?.image}
-            name={position.user?.display_name}
-            walletAddress={position.user?.wallet}
-            amount={formatBalance(BigInt(position.assets), 18, 4)}
-            feesAccrued={Number(
-              formatUnits(BigInt(+position.assets - +position.value), 18),
-            )}
-            updatedAt={position.updated_at}
-          />
-        </div>
-      ))}
+      <div className="mt-6 flex flex-col w-full">
+        {positions?.map((position) => (
+          <div
+            key={position.id}
+            className={`grow shrink basis-0 self-stretch p-6 bg-black first:rounded-t-xl last:rounded-b-xl border border-neutral-300/20 flex-col justify-start items-start gap-5 inline-flex`}
+          >
+            <IdentityPosition
+              variant="user"
+              avatarSrc={position.user?.image}
+              name={position.user?.display_name}
+              walletAddress={position.user?.wallet}
+              amount={formatBalance(BigInt(position.assets), 18, 4)}
+              feesAccrued={Number(
+                formatUnits(BigInt(+position.assets - +position.value), 18),
+              )}
+              updatedAt={position.updated_at}
+            />
+          </div>
+        ))}
+      </div>
       <Pagination className="flex w-full justify-between">
         <PaginationSummary
-          totalEntries={initialData.positionsPagination.total ?? 0}
+          totalEntries={pagination.total ?? 0}
           label="positions"
         />
         <div className="flex">
           <PaginationRowSelection defaultValue="10" />
           <PaginationPageCounter
-            currentPage={initialData.positionsPagination.page ?? 0}
-            totalPages={initialData.positionsPagination.totalPages ?? 0}
+            currentPage={pagination.page ?? 0}
+            totalPages={pagination.totalPages ?? 0}
           />
           <PaginationContent>
             <PaginationItem>
               <PaginationFirst
                 href="#"
                 onClick={() => onPageChange(1)}
-                disabled={initialData.positionsPagination.page === 1}
+                disabled={pagination.page === 1}
               />
             </PaginationItem>
             <PaginationItem>
               <PaginationPrevious
                 href="#"
-                onClick={() =>
-                  onPageChange(initialData.positionsPagination.page - 1)
-                }
+                onClick={() => onPageChange(pagination.page - 1)}
                 disabled={
-                  initialData.positionsPagination.page === 1 ||
-                  initialData.positionsPagination.page === undefined
+                  pagination.page === 1 || pagination.page === undefined
                 }
               />
             </PaginationItem>
             <PaginationItem>
               <PaginationNext
                 href="#"
-                onClick={() =>
-                  onPageChange(initialData.positionsPagination.page + 1)
-                }
-                disabled={
-                  initialData.positionsPagination.page ===
-                  initialData.positionsPagination.totalPages
-                }
+                onClick={() => onPageChange(pagination.page + 1)}
+                disabled={pagination.page === pagination.totalPages}
               />
             </PaginationItem>
             <PaginationItem>
               <PaginationLast
                 href="#"
-                onClick={() =>
-                  onPageChange(initialData.positionsPagination.totalPages)
-                }
-                disabled={
-                  initialData.positionsPagination.page ===
-                  initialData.positionsPagination.totalPages
-                }
+                onClick={() => onPageChange(pagination.totalPages)}
+                disabled={pagination.page === pagination.totalPages}
               />
             </PaginationItem>
           </PaginationContent>
