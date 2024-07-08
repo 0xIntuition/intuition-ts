a95cb4b34c3cc5a64f92e6c49b51e9a433e982c2
Rahul <37151383+rahulr8@users.noreply.github.com>
2024-07-08 11:56:15 -0700

    refactor

--- apps/portal/app/components/profile/data-created-header.tsx
+++ apps/portal/app/components/profile/data-created-header.tsx
@@ -1,9 +1,11 @@
-import { FeesAccrued, IdentityTag, MonetaryValue } from '@0xintuition/1ui'
+import { FeesAccrued, IdentityTag, MonetaryValue, Text } from '@0xintuition/1ui'
 
 import { formatBalance } from '@lib/utils/misc'
 
 interface DataCreatedHeaderProps {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   userIdentity: any
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   userTotals: any
 }
 
@@ -11,8 +13,6 @@ export const DataCreatedHeader: React.FC<DataCreatedHeaderProps> = ({
   userIdentity,
   userTotals,
 }) => {
-  console.log('userTotals', userTotals)
-
   const totalPositionValue = +formatBalance(
     userTotals?.total_position_value ?? '0',
     18,
@@ -22,46 +22,59 @@ export const DataCreatedHeader: React.FC<DataCreatedHeaderProps> = ({
   const feesAccrued = totalDelta - totalPositionValue
 
   return (
-    <div className="h-[184px] flex-col justify-start items-start gap-3 flex w-full">
-      <div className="self-stretch justify-start items-start gap-4 inline-flex">
-        <div className="grow shrink basis-0 self-stretch p-6 bg-black rounded-xl border border-neutral-300/20 flex-col justify-start items-start gap-5 inline-flex">
-          <div className="self-stretch justify-start items-start gap-5 inline-flex">
-            <div className="justify-start items-center gap-1.5 flex">
-              <div className="text-white/60 text-sm font-normal leading-tight">
-                Identities staked on by
-              </div>
-              <IdentityTag
-                imgSrc={userIdentity?.user?.image ?? userIdentity?.image}
-                variant={userIdentity?.user ? 'user' : 'non-user'}
+    <div className="h-46 flex flex-col w-full gap-3">
+      <div className="p-6 bg-black rounded-xl border border-neutral-300/20 flex flex-col gap-5">
+        <div className="flex gap-1.5 items-center">
+          <Text
+            variant="caption"
+            weight="regular"
+            className="text-secondary-foreground"
+          >
+            Identities staked on by
+          </Text>
+          <IdentityTag
+            imgSrc={userIdentity?.user?.image ?? userIdentity?.image}
+            variant={userIdentity?.user ? 'user' : 'non-user'}
+          >
+            <span className="min-w-20 text-ellipsis">
+              {userIdentity?.user?.display_name ?? userIdentity?.display_name}
+            </span>
+          </IdentityTag>
+        </div>
+        <div className="flex justify-between items-start">
+          <div className="flex gap-10">
+            <div className="flex flex-col items-start">
+              <Text
+                variant="caption"
+                weight="regular"
+                className="text-secondary-foreground"
               >
-                <span className="min-w-20 text-ellipsis">
-                  {userIdentity?.user?.display_name ??
-                    userIdentity?.display_name}
-                </span>
-              </IdentityTag>
-            </div>
-          </div>
-          <div className="self-stretch justify-between items-start inline-flex">
-            <div className="flex-col justify-start items-end inline-flex">
-              <div className="self-stretch text-white/60 text-xs font-normal leading-[18px]">
                 Identities
-              </div>
-              <div className="self-stretch text-white text-xl font-medium leading-[30px]">
+              </Text>
+              <div className="text-white text-xl font-medium">
                 {userTotals?.total_positions ?? '0'}
               </div>
             </div>
-            <div className="flex-col justify-start items-end inline-flex">
-              <div className="self-stretch text-white/60 text-xs font-normal leading-[18px]">
+            <div className="flex flex-col items-start">
+              <Text
+                variant="caption"
+                weight="regular"
+                className="text-secondary-foreground"
+              >
                 Total Staked
-              </div>
+              </Text>
               <MonetaryValue value={totalPositionValue} currency="ETH" />
             </div>
-            <div className="flex-col justify-start items-end inline-flex">
-              <div className="self-stretch text-right text-white/60 text-xs font-normal leading-[18px]">
-                Fees Accrued
-              </div>
-              <FeesAccrued value={feesAccrued} currency="ETH" />
-            </div>
+          </div>
+          <div className="flex flex-col items-end">
+            <Text
+              variant="caption"
+              weight="regular"
+              className="text-secondary-foreground"
+            >
+              Fees Accrued
+            </Text>
+            <FeesAccrued value={feesAccrued} currency="ETH" />
           </div>
         </div>
       </div>
