b84b2ad097d0a96c0a268b25b6855d83247044c4
Rahul <37151383+rahulr8@users.noreply.github.com>
2024-07-09 10:37:28 -0700

    wrap up

--- apps/portal/app/components/profile/connections-header.tsx
+++ apps/portal/app/components/profile/connections-header.tsx
@@ -1,8 +1,17 @@
-import { FeesAccrued, IdentityTag, MonetaryValue, Text } from '@0xintuition/1ui'
+import { Claim, MonetaryValue, Text } from '@0xintuition/1ui'
 
 import { formatBalance } from '@lib/utils/misc'
 
+export const ConnectionsHeaderVariants = {
+  followers: 'followers',
+  following: 'following',
+} as const
+
+export type ConnectionsHeaderVariantType =
+  (typeof ConnectionsHeaderVariants)[keyof typeof ConnectionsHeaderVariants]
+
 interface ConnectionsHeaderProps {
+  variant: ConnectionsHeaderVariantType
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   userIdentity: any
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -10,6 +19,7 @@ interface ConnectionsHeaderProps {
 }
 
 export const ConnectionsHeader: React.FC<ConnectionsHeaderProps> = ({
+  variant,
   userIdentity,
   userTotals,
 }) => {
@@ -18,29 +28,10 @@ export const ConnectionsHeader: React.FC<ConnectionsHeaderProps> = ({
     18,
     4,
   )
-  const totalDelta = +formatBalance(userTotals?.total_delta ?? '0', 18, 4)
-  const feesAccrued = totalDelta - totalPositionValue
 
   return (
-    <div className="h-46 flex flex-col w-full gap-3">
+    <div className="flex flex-col w-full gap-3">
       <div className="p-6 bg-black rounded-xl border border-neutral-300/20 flex flex-col gap-5">
-        <div className="flex gap-1.5 items-center">
-          <Text
-            variant="caption"
-            weight="regular"
-            className="text-secondary-foreground"
-          >
-            Identities staked on by
-          </Text>
-          <IdentityTag
-            imgSrc={userIdentity?.user?.image ?? userIdentity?.image}
-            variant={userIdentity?.user ? 'user' : 'non-user'}
-          >
-            <span className="min-w-20 text-ellipsis">
-              {userIdentity?.user?.display_name ?? userIdentity?.display_name}
-            </span>
-          </IdentityTag>
-        </div>
         <div className="flex justify-between items-start">
           <div className="flex gap-10">
             <div className="flex flex-col items-start">
@@ -49,7 +40,7 @@ export const ConnectionsHeader: React.FC<ConnectionsHeaderProps> = ({
                 weight="regular"
                 className="text-secondary-foreground"
               >
-                Identities
+                {variant === 'followers' ? 'Followers' : 'Following'}
               </Text>
               <div className="text-white text-xl font-medium">
                 {userTotals?.total_positions ?? '0'}
@@ -61,7 +52,9 @@ export const ConnectionsHeader: React.FC<ConnectionsHeaderProps> = ({
                 weight="regular"
                 className="text-secondary-foreground"
               >
-                Total Staked
+                {variant === 'followers'
+                  ? 'Total stake in the Follow Claim'
+                  : 'Total stake'}
               </Text>
               <MonetaryValue value={totalPositionValue} currency="ETH" />
             </div>
@@ -72,9 +65,23 @@ export const ConnectionsHeader: React.FC<ConnectionsHeaderProps> = ({
               weight="regular"
               className="text-secondary-foreground"
             >
-              Fees Accrued
+              Follow Claim
             </Text>
-            <FeesAccrued value={feesAccrued} currency="ETH" />
+            {/* TODO: Insert actual values here */}
+            <Claim
+              subject={{
+                variant: 'non-user',
+                label: userIdentity?.name,
+              }}
+              predicate={{
+                variant: 'non-user',
+                label: 'is really',
+              }}
+              object={{
+                variant: 'non-user',
+                label: 'cool',
+              }}
+            />
           </div>
         </div>
       </div>
