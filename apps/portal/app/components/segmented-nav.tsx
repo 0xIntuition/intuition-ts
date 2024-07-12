cc0d627edd83c423ee8a5d0d1964e7597f3039e8
Vitalsine85 <rob@creative-element.net>
2024-07-11 23:43:54 -0300

    Fix spacing and scrolling on main nested layout

--- apps/portal/app/components/segmented-nav.tsx
+++ apps/portal/app/components/segmented-nav.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-import { SegmentedControl, SegmentedControlItem } from '@0xintuition/1ui'
+import { SegmentedControl, SegmentedControlItem, Text } from '@0xintuition/1ui'
 
 import { useNavigate, useParams } from '@remix-run/react'
 
@@ -58,7 +58,7 @@ export const SegmentedNav = ({ options }: SegmentedNavProps) => {
           isActive={selectedTab === option.value}
           onClick={() => handleTabClick(option)}
         >
-          {option.label}
+          <Text variant="small">{option.label}</Text>
         </SegmentedControlItem>
       ))}
     </SegmentedControl>
