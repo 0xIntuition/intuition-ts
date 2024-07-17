091ba990e167bf4ae7cd34744b83d81bc0abd0c4
alexander-mann <alexandercmann@gmail.com>
2024-07-17 16:28:43 -0400

    push

--- apps/portal/app/components/explore/ExploreSearchForm/ExploreAddTags/ExploreAddTags.tsx
+++ apps/portal/app/components/explore/ExploreSearchForm/ExploreAddTags/ExploreAddTags.tsx
@@ -6,29 +6,24 @@ import {
   PopoverTrigger,
   TagsListInput,
 } from '@0xintuition/1ui'
-import { IdentityPresenter, SortColumn, SortDirection } from '@0xintuition/api'
+import { IdentityPresenter } from '@0xintuition/api'
 
 import { IdentitySearchCombobox } from '@components/identity/identity-search-combo-box'
-import { fetchIdentities, fetchIdentity } from '@lib/utils/fetches'
+import { useIdentityServerSearch } from '@lib/hooks/useIdentityServerSearch'
+import { fetchIdentity } from '@lib/utils/fetches'
 import { TagType } from 'types/tags'
 
 import {
-  filterSearchResults,
   isClickOutsideOfTagsInteractionZone,
   isTagAlreadySelected,
 } from './ExploreAddTags.utils'
 
 const ExploreAddTags = ({ initialValue }: { initialValue?: string | null }) => {
+  const { setSearchQuery, identities, handleInput } = useIdentityServerSearch()
   const tagsContainerRef = React.useRef<HTMLDivElement>(null)
   const popoverContentRef = React.useRef<HTMLDivElement>(null)
   const [isPopoverOpen, setIsPopoverOpen] = React.useState(false)
   const [selectedTags, setSelectedTags] = React.useState<TagType[]>([])
-  const [identityResults, setIdentityResults] = React.useState<
-    IdentityPresenter[]
-  >([])
-  const [displayResults, setDisplayResults] = React.useState<
-    IdentityPresenter[]
-  >([])
   const [formElementValue, setFormElementValue] = React.useState('')
 
   React.useEffect(() => {
@@ -51,7 +46,11 @@ const ExploreAddTags = ({ initialValue }: { initialValue?: string | null }) => {
       })
       setSelectedTags(initialSelectedTags)
     }
+    // Only run this block once on load
+    // eslint-disable-next-line
+  }, [])
 
+  React.useEffect(() => {
     document.addEventListener('click', (event) => {
       if (
         isPopoverOpen &&
@@ -64,23 +63,13 @@ const ExploreAddTags = ({ initialValue }: { initialValue?: string | null }) => {
         setIsPopoverOpen(false)
       }
     })
-    // Only run this block once on load
-    // eslint-disable-next-line
-  }, [])
+  })
 
   React.useEffect(() => {
     const selectedTagIds: string[] = []
     selectedTags.forEach((tag) => selectedTagIds.push(tag.id))
     setFormElementValue(selectedTagIds.toString())
   }, [selectedTags])
-
-  const handleTagSelectionChange = (
-    selectedTags: TagType[],
-    results: IdentityPresenter[],
-  ) => {
-    setSelectedTags(selectedTags)
-    setDisplayResults(filterSearchResults(selectedTags, results))
-  }
 
   return (
     <div ref={tagsContainerRef}>
@@ -97,10 +86,9 @@ const ExploreAddTags = ({ initialValue }: { initialValue?: string | null }) => {
           tags={selectedTags}
           maxTags={5}
           onAddTag={() => setIsPopoverOpen(true)}
-          onRemoveTag={(id: string) => {
-            const updatedTagsList = selectedTags.filter((tag) => tag.id !== id)
-            handleTagSelectionChange(updatedTagsList, identityResults)
-          }}
+          onRemoveTag={(id: string) =>
+            setSelectedTags(selectedTags.filter((tag) => tag.id !== id))
+          }
         />
         <PopoverTrigger className="block" />
         <PopoverContent
@@ -109,35 +97,20 @@ const ExploreAddTags = ({ initialValue }: { initialValue?: string | null }) => {
         >
           <IdentitySearchCombobox
             placeholder="Search for a tag..."
-            identities={displayResults}
-            onKeyDown={async (event) => {
-              const target = event.target as HTMLInputElement
-              // TODO: Update logic when functionality is available [ENG-2519]
-              const searchResults = await fetchIdentities(
-                undefined,
-                20, // TODO: Determine appropriate results limit [ENG-2519]
-                SortColumn.DISPLAY_NAME,
-                SortDirection.ASC,
-                target.value,
-              )
-              if (searchResults?.data) {
-                // keep original results in case a tag is de-selected
-                setIdentityResults(searchResults.data)
-                setDisplayResults(
-                  filterSearchResults(selectedTags, identityResults),
-                )
-              }
-            }}
+            identities={identities.filter(
+              (identity) => !selectedTags.some((tag) => tag.id === identity.id),
+            )}
             onIdentitySelect={(selection: IdentityPresenter) => {
               if (!isTagAlreadySelected(selection, selectedTags)) {
-                const updatedTagsList = [
+                setSelectedTags([
                   ...selectedTags,
                   { name: selection.display_name, id: selection.id },
-                ]
-                handleTagSelectionChange(updatedTagsList, displayResults)
+                ])
               }
-              // TODO: Determine whether we should be launching a toast should this hit an else [ENG-2519]
             }}
+            onValueChange={setSearchQuery}
+            onInput={handleInput}
+            shouldFilter={false}
           />
         </PopoverContent>
       </Popover>
--- apps/portal/app/components/explore/ExploreSearchForm/ExploreAddTags/ExploreAddTags.utils.ts
+++ apps/portal/app/components/explore/ExploreSearchForm/ExploreAddTags/ExploreAddTags.utils.ts
@@ -6,20 +6,16 @@ export const isClickOutsideOfTagsInteractionZone = (
   tagsContainerRef: React.RefObject<HTMLDivElement>,
   popoverContentRef: React.RefObject<HTMLDivElement>,
   target: EventTarget | null,
-) =>
-  target &&
-  tagsContainerRef.current &&
-  !tagsContainerRef.current.contains(target as Node) &&
-  popoverContentRef.current &&
-  !popoverContentRef.current.contains(target as Node)
+) => {
+  console.log(!tagsContainerRef.current?.contains(target as Node))
+  console.log(!popoverContentRef.current?.contains(target as Node))
+  return (
+    !tagsContainerRef.current?.contains(target as Node) &&
+    !popoverContentRef.current?.contains(target as Node)
+  )
+}
 
 export const isTagAlreadySelected = (
   selection: IdentityPresenter,
   selectedTags: TagType[],
 ) => selectedTags.filter((tag) => tag.id === selection.id).length > 0
-
-export const filterSearchResults = (
-  selectedTags: TagType[],
-  results: IdentityPresenter[],
-) =>
-  results.filter((result) => !selectedTags.some((tag) => tag.id === result.id))
