ccec0c987b8e07712f91ad9feee26047773b2d1f
0xjojikun <jojikun@intuition.systems>
2024-07-17 15:56:54 -0600

    fix: Update svg bg to inline

--- packages/1ui/src/components/ProgressCard/ProgressCard.tsx
+++ packages/1ui/src/components/ProgressCard/ProgressCard.tsx
@@ -24,7 +24,22 @@ const ProgressCard = ({
       {...props}
     >
       <div className="absolute top-0 left-14">
-        <ProgressBg className="fill-muted-foreground/70" />
+        {/* TODO: ENG-0000 Abstract out SVG to tailwind config */}
+        <svg
+          width="458"
+          height="110"
+          viewBox="0 0 458 110"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className={cn(
+            `overflow-visible blur-[75px] fill-muted-foreground/70`,
+          )}
+        >
+          <path
+            d="M322.494 41.6458L278.681 4.9424C278.665 4.92897 278.65 4.91391 278.637 4.89746L274.89 0.114968C274.833 0.0423948 274.746 0 274.654 0H0.3C0.134315 0 0 0.134312 0 0.299998V49.5968C0 49.7551 0.123012 49.8862 0.281004 49.8962L113.74 57.0953C113.764 57.0968 113.789 57.1014 113.812 57.1088L203.399 85.6306C203.432 85.6411 203.466 85.6458 203.5 85.6446L289.221 82.6945C289.249 82.6936 289.277 82.6965 289.304 82.7034L421.738 115.974C421.805 115.991 421.876 115.984 421.939 115.954L457.393 99.2227C457.628 99.1121 457.621 98.7761 457.382 98.6751L322.57 41.6922C322.542 41.6806 322.517 41.665 322.494 41.6458Z"
+            className="currentColor"
+          />
+        </svg>
       </div>
       <div className="space-y-8">
         <div className="flex w-full justify-between align-items">
@@ -55,33 +70,6 @@ const ProgressCard = ({
         </div>
       </div>
     </div>
-  )
-}
-
-export const ProgressBg = ({
-  className,
-  ...props
-}: React.SVGProps<SVGSVGElement>) => {
-  return (
-    <svg
-      width="458"
-      height="110"
-      viewBox="0 0 458 110"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      className={cn(
-        `overflow-visible blur-[75px] fill-muted-foreground/70`,
-        className,
-      )}
-      {...props}
-    >
-      <g>
-        <path
-          d="M322.494 41.6458L278.681 4.9424C278.665 4.92897 278.65 4.91391 278.637 4.89746L274.89 0.114968C274.833 0.0423948 274.746 0 274.654 0H0.3C0.134315 0 0 0.134312 0 0.299998V49.5968C0 49.7551 0.123012 49.8862 0.281004 49.8962L113.74 57.0953C113.764 57.0968 113.789 57.1014 113.812 57.1088L203.399 85.6306C203.432 85.6411 203.466 85.6458 203.5 85.6446L289.221 82.6945C289.249 82.6936 289.277 82.6965 289.304 82.7034L421.738 115.974C421.805 115.991 421.876 115.984 421.939 115.954L457.393 99.2227C457.628 99.1121 457.621 98.7761 457.382 98.6751L322.57 41.6922C322.542 41.6806 322.517 41.665 322.494 41.6458Z"
-          className="currentColor"
-        />
-      </g>
-    </svg>
   )
 }
 
