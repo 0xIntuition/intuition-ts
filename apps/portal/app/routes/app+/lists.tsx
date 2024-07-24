64c23dda8ea887f4c7d2884905f1315cf3c665e6
jonathanprozzi <jonathanprozzi@gmail.com>
2024-07-24 13:32:56 -0400

    Adds the buttons for Explore and Create a List

--- apps/portal/app/routes/app+/lists.tsx
+++ apps/portal/app/routes/app+/lists.tsx
@@ -1,4 +1,6 @@
 import {
+  Button,
+  Icon,
   Tabs,
   TabsContent,
   TabsList,
@@ -11,14 +13,14 @@ import {
   ClaimsService,
 } from '@0xintuition/api'
 
-import { ExploreSearch } from '@components/explore/ExploreSearch'
 import { ListClaimsList } from '@components/list/list-claims'
 import { TAG_PREDICATE_VAULT_ID_TESTNET } from '@lib/utils/constants'
 import { NO_WALLET_ERROR } from '@lib/utils/errors'
+import logger from '@lib/utils/logger'
 import { calculateTotalPages, fetchWrapper, invariant } from '@lib/utils/misc'
 import { getStandardPageParams } from '@lib/utils/params'
 import { json, LoaderFunctionArgs } from '@remix-run/node'
-import { useLoaderData } from '@remix-run/react'
+import { useLoaderData, useNavigate } from '@remix-run/react'
 import { requireUserWallet } from '@server/auth'
 
 export async function loader({ request }: LoaderFunctionArgs) {
@@ -61,26 +63,46 @@ export async function loader({ request }: LoaderFunctionArgs) {
 
 export default function ListsRoute() {
   const { listClaims, pagination } = useLoaderData<typeof loader>()
+  const navigate = useNavigate()
 
   return (
     <div className="m-8 flex flex-col">
-      <div className="flex flex-col mb-10">
-        <Text
-          variant="headline"
-          weight="medium"
-          className="theme-secondary-foreground w-full"
-        >
-          Lists
-        </Text>
-        <Text
-          variant="footnote"
-          weight="regular"
-          className="theme-secondary-foreground/50"
-        >
-          Begin the process of establishing a new digital
-          <br />
-          representation within the blockchain network.
-        </Text>
+      <div className="flex flex-row justify-between items-start">
+        <div className="flex flex-col mb-10">
+          <Text
+            variant="headline"
+            weight="medium"
+            className="theme-secondary-foreground w-full"
+          >
+            Lists
+          </Text>
+          <Text
+            variant="footnote"
+            weight="regular"
+            className="theme-secondary-foreground/50"
+          >
+            Begin the process of establishing a new digital
+            <br />
+            representation within the blockchain network.
+          </Text>
+        </div>
+        <div className="flex flex-row gap-2.5">
+          <Button
+            variant="secondary"
+            onClick={() => navigate(`/app/explore/lists`)}
+          >
+            <Icon name="magnifying-glass" className="mr-2" />
+            Explore
+          </Button>
+          <Button variant="secondary">
+            <Icon
+              name="plus-small"
+              className="mr-2"
+              onClick={() => logger('create new list clicked')} // TODO: [ENG-2798] - add the create list functionality
+            />
+            Create new list
+          </Button>
+        </div>
       </div>
       <Tabs defaultValue="saved">
         <TabsList>
