f3ab265cceca7aca0cbad22833bbb0f133943b96
0xjojikun <jojikun@intuition.systems>
2024-07-24 00:08:43 -0600

    squash

--- apps/portal/app/lib/utils/quest.tsx
+++ apps/portal/app/lib/utils/quest.tsx
@@ -28,3 +28,22 @@ export function getQuestCriteria(condition: QuestCondition) {
       return 'Unknown'
   }
 }
+
+export function getQuestImage(questId: string) {
+  switch (questId) {
+    case 'b4b7e0ed-a69a-4a0a-bf9b-8bf0afe979f9':
+      return 'https://res.cloudinary.com/dfpwy9nyv/image/upload/v1721799356/Portal%20Assets/quests/create-atom.png'
+    case '3b5315bb-f29b-4320-9997-6928b4bbf3e9':
+      return 'https://res.cloudinary.com/dfpwy9nyv/image/upload/v1721799356/Portal%20Assets/quests/stake-atom.png'
+    case 'dacdd63d-f978-433e-aeb0-54d165e8f936':
+      return 'https://res.cloudinary.com/dfpwy9nyv/image/upload/v1721799356/Portal%20Assets/quests/create-claim.png'
+    case 'ac22e180-9923-4481-9cba-31e88f223e58':
+      return 'https://res.cloudinary.com/dfpwy9nyv/image/upload/v1721799356/Portal%20Assets/quests/stake-claim.png'
+    case 'daa5d6ff-9a8b-41c6-a259-302816655c5b':
+      return 'https://res.cloudinary.com/dfpwy9nyv/image/upload/v1721799356/Portal%20Assets/quests/counter-stake-claim.png'
+    case '90cf4398-f7e5-4ec7-8088-c64d2da6a715':
+      return 'https://res.cloudinary.com/dfpwy9nyv/image/upload/v1721799356/Portal%20Assets/quests/discovery.png'
+    default:
+      return 'https://res.cloudinary.com/dfpwy9nyv/image/upload/v1721799356/Portal%20Assets/quests/create-atom.png'
+  }
+}
