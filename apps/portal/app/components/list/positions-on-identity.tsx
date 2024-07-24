4220194b666795aced1d919c688712656fdca331
Vitalsine85 <rob@creative-element.net>
2024-07-24 13:20:48 -0400

    update data about route with new skeleton layout

--- apps/portal/app/components/list/positions-on-identity.tsx
+++ apps/portal/app/components/list/positions-on-identity.tsx
@@ -34,28 +34,32 @@ export function PositionsOnIdentity({
       options={options}
       paramPrefix="positions"
     >
-      {positions?.map((position) => (
-        <div
-          key={position.id}
-          className={`grow shrink basis-0 self-stretch p-6 bg-black first:rounded-t-xl last:rounded-b-xl border border-neutral-300/20 flex-col justify-start items-start gap-5 inline-flex`}
-        >
-          <IdentityPosition
-            variant={Identity.user}
-            avatarSrc={position.user?.image ?? ''}
-            name={position.user?.display_name ?? ''}
-            walletAddress={position.user?.wallet ?? ''}
-            amount={+formatBalance(BigInt(position.assets), 18, 4)}
-            feesAccrued={Number(
-              formatUnits(BigInt(+position.assets - +position.value), 18),
-            )}
-            updatedAt={position.updated_at}
-            onClick={() => {
-              navigate(`/app/identity/${position.user?.wallet}`)
-            }}
-            className="hover:cursor-pointer"
-          />
-        </div>
-      ))}
+      {!positions.length ? (
+        <EmptyStateCard message="No positions found." />
+      ) : (
+        positions.map((position) => (
+          <div
+            key={position.id}
+            className="grow shrink basis-0 self-stretch p-6 bg-black first:rounded-t-xl last:rounded-b-xl border border-neutral-300/20 flex-col justify-start items-start gap-5 inline-flex"
+          >
+            <IdentityPosition
+              variant={Identity.user}
+              avatarSrc={position.user?.image ?? ''}
+              name={position.user?.display_name ?? ''}
+              walletAddress={position.user?.wallet ?? ''}
+              amount={+formatBalance(BigInt(position.assets), 18, 4)}
+              feesAccrued={Number(
+                formatUnits(BigInt(+position.assets - +position.value), 18),
+              )}
+              updatedAt={position.updated_at}
+              onClick={() => {
+                navigate(`/app/identity/${position.user?.wallet}`)
+              }}
+              className="hover:cursor-pointer"
+            />
+          </div>
+        ))
+      )}
     </List>
   )
 }
