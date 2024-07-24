e90eebceca044deca3472ad10d93279ee5c30947
Vitalsine85 <rob@creative-element.net>
2024-07-24 15:21:29 -0400

    use rahuls length check

--- apps/portal/app/components/list/claims.tsx
+++ apps/portal/app/components/list/claims.tsx
@@ -46,58 +46,54 @@ export function ClaimsList({
       enableSearch={enableSearch}
       enableSort={enableSort}
     >
-      {!claims.length ? (
-        <EmptyStateCard message="No claims found." />
-      ) : (
-        claims.map((claim) => (
-          <div
-            key={claim.claim_id}
-            className="grow shrink basis-0 self-stretch p-6 bg-background first:rounded-t-xl last:rounded-b-xl theme-border flex-col justify-start gap-5 inline-flex"
+      {claims?.map((claim) => (
+        <div
+          key={claim.claim_id}
+          className="grow shrink basis-0 self-stretch p-6 bg-background first:rounded-t-xl last:rounded-b-xl theme-border flex-col justify-start gap-5 inline-flex"
+        >
+          <ClaimRow
+            claimsFor={claim.for_num_positions}
+            claimsAgainst={claim.against_num_positions}
+            amount={+formatBalance(claim.assets_sum, 18, 4)}
+            onClick={() => {
+              navigate(`/app/claim/${claim.claim_id}`)
+            }}
+            className="hover:cursor-pointer"
           >
-            <ClaimRow
-              claimsFor={claim.for_num_positions}
-              claimsAgainst={claim.against_num_positions}
-              amount={+formatBalance(claim.assets_sum, 18, 4)}
-              onClick={() => {
-                navigate(`/app/claim/${claim.claim_id}`)
+            <Claim
+              subject={{
+                variant: claim.subject?.is_user
+                  ? Identity.user
+                  : Identity.nonUser,
+                label:
+                  claim.subject?.user?.display_name ??
+                  claim.subject?.display_name ??
+                  claim.subject?.identity_id ??
+                  '',
+                imgSrc: claim.subject?.image,
               }}
-              className="hover:cursor-pointer"
-            >
-              <Claim
-                subject={{
-                  variant: claim.subject?.is_user
-                    ? Identity.user
-                    : Identity.nonUser,
-                  label:
-                    claim.subject?.user?.display_name ??
-                    claim.subject?.display_name ??
-                    claim.subject?.identity_id ??
-                    '',
-                  imgSrc: claim.subject?.image,
-                }}
-                predicate={{
-                  variant: claim.predicate?.is_user ? 'user' : 'non-user',
-                  label:
-                    claim.predicate?.user?.display_name ??
-                    claim.predicate?.display_name ??
-                    claim.predicate?.identity_id ??
-                    '',
-                  imgSrc: claim.predicate?.image,
-                }}
-                object={{
-                  variant: claim.object?.is_user ? 'user' : 'non-user',
-                  label:
-                    claim.object?.user?.display_name ??
-                    claim.object?.display_name ??
-                    claim.object?.identity_id ??
-                    '',
-                  imgSrc: claim.object?.image,
-                }}
-              />
-            </ClaimRow>
-          </div>
-        ))
-      )}
+              predicate={{
+                variant: claim.predicate?.is_user ? 'user' : 'non-user',
+                label:
+                  claim.predicate?.user?.display_name ??
+                  claim.predicate?.display_name ??
+                  claim.predicate?.identity_id ??
+                  '',
+                imgSrc: claim.predicate?.image,
+              }}
+              object={{
+                variant: claim.object?.is_user ? 'user' : 'non-user',
+                label:
+                  claim.object?.user?.display_name ??
+                  claim.object?.display_name ??
+                  claim.object?.identity_id ??
+                  '',
+                imgSrc: claim.object?.image,
+              }}
+            />
+          </ClaimRow>
+        </div>
+      ))}
     </List>
   )
 }
--- apps/portal/app/components/list/positions-on-identity.tsx
+++ apps/portal/app/components/list/positions-on-identity.tsx
@@ -24,7 +24,7 @@ export function PositionsOnIdentity({
   ]
 
   if (!positions.length) {
-    return <EmptyStateCard message="No positions found on this identity." />
+    return <EmptyStateCard message="No positions found." />
   }
 
   return (
@@ -34,32 +34,28 @@ export function PositionsOnIdentity({
       options={options}
       paramPrefix="positions"
     >
-      {!positions.length ? (
-        <EmptyStateCard message="No positions found." />
-      ) : (
-        positions.map((position) => (
-          <div
-            key={position.id}
-            className="grow shrink basis-0 self-stretch p-6 bg-black first:rounded-t-xl last:rounded-b-xl border border-neutral-300/20 flex-col justify-start items-start gap-5 inline-flex"
-          >
-            <IdentityPosition
-              variant={Identity.user}
-              avatarSrc={position.user?.image ?? ''}
-              name={position.user?.display_name ?? ''}
-              walletAddress={position.user?.wallet ?? ''}
-              amount={+formatBalance(BigInt(position.assets), 18, 4)}
-              feesAccrued={Number(
-                formatUnits(BigInt(+position.assets - +position.value), 18),
-              )}
-              updatedAt={position.updated_at}
-              onClick={() => {
-                navigate(`/app/identity/${position.user?.wallet}`)
-              }}
-              className="hover:cursor-pointer"
-            />
-          </div>
-        ))
-      )}
+      {positions?.map((position) => (
+        <div
+          key={position.id}
+          className={`grow shrink basis-0 self-stretch p-6 bg-black first:rounded-t-xl last:rounded-b-xl border border-neutral-300/20 flex-col justify-start items-start gap-5 inline-flex`}
+        >
+          <IdentityPosition
+            variant={Identity.user}
+            avatarSrc={position.user?.image ?? ''}
+            name={position.user?.display_name ?? ''}
+            walletAddress={position.user?.wallet ?? ''}
+            amount={+formatBalance(BigInt(position.assets), 18, 4)}
+            feesAccrued={Number(
+              formatUnits(BigInt(+position.assets - +position.value), 18),
+            )}
+            updatedAt={position.updated_at}
+            onClick={() => {
+              navigate(`/app/identity/${position.user?.wallet}`)
+            }}
+            className="hover:cursor-pointer"
+          />
+        </div>
+      ))}
     </List>
   )
 }
