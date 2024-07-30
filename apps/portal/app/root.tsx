be12cae4e0f67e20607a095d92083b3af7705e6b
alexander-mann <alexandercmann@gmail.com>
2024-07-30 08:58:49 -0400

    remove red color of icon

--- apps/portal/app/root.tsx
+++ apps/portal/app/root.tsx
@@ -154,7 +154,7 @@ export function ErrorBoundary() {
   const error = useRouteError()
   let statusCode = null
   let title: string | React.ReactNode = (
-    <Icon name="circle-x" className="h-20 w-20 text-destructive" />
+    <Icon name="circle-x" className="h-20 w-20" />
   )
   let description = 'Something went wrong...'
 
