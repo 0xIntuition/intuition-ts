893e589c71cb570fe0cf5c9564149c3ebaceb849
Vitalsine85 <rob@creative-element.net>
2024-08-01 10:55:11 -0400

    add useUpdateApiHeaders

---
+++ apps/portal/app/lib/hooks/useUpdateApiHeaders.tsx
@@ -0,0 +1,28 @@
+// In a new file, e.g., apps/portal/app/hooks/useUpdateApiHeaders.ts
+
+import { useEffect } from 'react'
+
+import { OpenAPI } from '@0xintuition/api'
+
+import logger from '@lib/utils/logger'
+import { getAuthHeaders } from '@lib/utils/misc'
+import { useLocation, useMatches } from '@remix-run/react'
+
+export function useUpdateApiHeaders() {
+  const matches = useMatches()
+  const location = useLocation()
+
+  useEffect(() => {
+    const updateHeaders = async () => {
+      const accessToken = localStorage.getItem('privy:token')
+      logger('accessToken from useUpdateApiHeaders', accessToken)
+
+      if (accessToken) {
+        const headers = getAuthHeaders(accessToken)
+        OpenAPI.HEADERS = headers as Record<string, string>
+      }
+    }
+
+    updateHeaders()
+  }, [matches, location.search])
+}
