2b63d5897b06f7a0aaadec0006f948ddc7a78fbd
alexander-mann <alexandercmann@gmail.com>
2024-08-01 11:14:32 -0400

    fix it up

--- packages/1ui/src/components/SidebarLayout/components/SidebarLayoutNav.tsx
+++ packages/1ui/src/components/SidebarLayout/components/SidebarLayoutNav.tsx
@@ -23,7 +23,7 @@ export const SidebarLayoutNav = ({
   return (
     <div
       className={cn(
-        'theme-border border-0 h-screen',
+        'theme-border border-0',
         !isMobileView && 'border-r-px transition-all duration-300 ease-in-out',
         className,
       )}
