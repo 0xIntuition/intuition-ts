4cb7095cddda3e073e524fbf22281cbcb139f2be
jonathanprozzi <jonathanprozzi@gmail.com>
2024-08-07 01:14:27 -0400

    Adds in claims and modifies list.tsx to support optional pagination for when we dont want to show it such as in this view

--- apps/portal/app/routes/app+/profile+/$wallet+/index.tsx
+++ apps/portal/app/routes/app+/profile+/$wallet+/index.tsx
@@ -1,4 +1,6 @@
-import { Text } from '@0xintuition/1ui'
+import { Suspense } from 'react'
+
+import { ErrorStateCard, Text } from '@0xintuition/1ui'
 import {
   ClaimSortColumn,
   ClaimsService,
@@ -7,18 +9,22 @@ import {
   UserTotalsPresenter,
 } from '@0xintuition/api'
 
+import { ClaimsList as ClaimsAboutIdentity } from '@components/list/claims'
 import { ListClaimsList } from '@components/list/list-claims'
+import { ListClaimsSkeletonLayout } from '@components/list/list-skeletons'
 import { OverviewAboutHeader } from '@components/profile/overview-about-header'
 import { OverviewCreatedHeader } from '@components/profile/overview-created-header'
 import { OverviewStakingHeader } from '@components/profile/overview-staking-header'
+import { RevalidateButton } from '@components/revalidate-button'
+import { DataHeaderSkeleton, PaginatedListSkeleton } from '@components/skeleton'
 import { useLiveLoader } from '@lib/hooks/useLiveLoader'
 import { getClaimsAboutIdentity } from '@lib/services/claims'
 import { getUserSavedLists } from '@lib/services/lists'
 import { getPositionsOnIdentity } from '@lib/services/positions'
 import logger from '@lib/utils/logger'
 import { formatBalance, invariant } from '@lib/utils/misc'
-import { json, LoaderFunctionArgs } from '@remix-run/node'
-import { useRouteLoaderData } from '@remix-run/react'
+import { defer, LoaderFunctionArgs } from '@remix-run/node'
+import { Await, useRouteLoaderData } from '@remix-run/react'
 import { fetchWrapper } from '@server/api'
 import { requireUserWallet } from '@server/auth'
 import {
@@ -43,24 +49,43 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
   listSearchParams.set('direction', SortDirection.DESC)
   listSearchParams.set('limit', '6')
 
-  return json({
-    positions: await getPositionsOnIdentity({
+  const claimSearchParams = new URLSearchParams()
+  claimSearchParams.set('sortBy', ClaimSortColumn.ASSETS_SUM)
+  claimSearchParams.set('direction', SortDirection.DESC)
+  claimSearchParams.set('limit', '5')
+
+  // const [positions, claimsSummary] = await Promise.all([
+  //   getPositionsOnIdentity({
+  //     request,
+  //     identityId: wallet,
+  //     searchParams,
+  //   }),
+  //   fetchWrapper(request, {
+  //     method: ClaimsService.claimSummary,
+  //     args: {
+  //       identity: wallet,
+  //     },
+  //   }),
+  // ])
+
+  return defer({
+    positions: getPositionsOnIdentity({
       request,
       identityId: wallet,
       searchParams,
     }),
-    claims: await getClaimsAboutIdentity({
-      request,
-      identityId: wallet,
-      searchParams,
-    }),
-    claimsSummary: await fetchWrapper(request, {
+    claimsSummary: fetchWrapper(request, {
       method: ClaimsService.claimSummary,
       args: {
         identity: wallet,
       },
     }),
-    savedListClaims: await getUserSavedLists({
+    claims: getClaimsAboutIdentity({
+      request,
+      identityId: wallet,
+      searchParams: claimSearchParams,
+    }),
+    savedListClaims: getUserSavedLists({
       request,
       userWallet: wallet,
       searchParams: listSearchParams,
@@ -100,13 +125,24 @@ export default function ProfileOverview() {
           }
           link={`${PATHS.PROFILE}/data-created`}
         />
-        <OverviewAboutHeader
-          variant="claims"
-          userIdentity={userIdentity}
-          totalClaims={claims.pagination?.totalEntries}
-          totalStake={+formatBalance(claimsSummary?.assets_sum ?? 0, 18, 4)}
-          link={`${PATHS.PROFILE}/data-about`}
-        />
+        <Suspense fallback={<DataHeaderSkeleton />}>
+          <Await
+            resolve={Promise.all([claims, claimsSummary])}
+            errorElement={<></>}
+          >
+            {([resolvedClaims, resolvedClaimsSummary]) => (
+              <OverviewAboutHeader
+                variant="claims"
+                userIdentity={userIdentity}
+                totalClaims={resolvedClaims.pagination?.totalEntries}
+                totalStake={
+                  +formatBalance(resolvedClaimsSummary?.assets_sum ?? 0, 18, 4)
+                }
+                link={`${PATHS.PROFILE}/data-about`}
+              />
+            )}
+          </Await>
+        </Suspense>
       </div>
       <div className="flex flex-row items-center gap-6 max-md:flex-col">
         <OverviewCreatedHeader
@@ -125,14 +161,55 @@ export default function ProfileOverview() {
         weight="medium"
         className="text-secondary-foreground"
       >
+        Top Claims about this Identity
+      </Text>
+      <Suspense fallback={<PaginatedListSkeleton />}>
+        <Await
+          resolve={claims}
+          errorElement={
+            <ErrorStateCard>
+              <RevalidateButton />
+            </ErrorStateCard>
+          }
+        >
+          {(resolvedClaims) => (
+            <ClaimsAboutIdentity
+              claims={resolvedClaims.data}
+              paramPrefix="claims"
+              enableSearch={false}
+              enableSort={false}
+            />
+          )}
+        </Await>
+      </Suspense>
+      <Text
+        variant="headline"
+        weight="medium"
+        className="text-secondary-foreground"
+      >
+        Top Followers
+      </Text>
+      <Text
+        variant="headline"
+        weight="medium"
+        className="text-secondary-foreground"
+      >
         Top Lists
       </Text>
-      <ListClaimsList
-        listClaims={savedListClaims.savedListClaims}
-        enableSort={false}
-        enableSearch={false}
-        columns={3}
-      />
+      <Suspense fallback={<ListClaimsSkeletonLayout totalItems={6} />}>
+        <Await resolve={savedListClaims}>
+          {(resolvedSavedListClaims) => {
+            return (
+              <ListClaimsList
+                listClaims={resolvedSavedListClaims.savedListClaims}
+                enableSort={false}
+                enableSearch={false}
+                columns={3}
+              />
+            )
+          }}
+        </Await>
+      </Suspense>
     </div>
   )
 }
