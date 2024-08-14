9dbe8645f8b1ab9d66088a6cc85d2774a2825bcb
Jonathan Prozzi <jonathanprozzi@gmail.com>
2024-08-13 22:39:05 -0400

    Update apps/portal/app/components/list/save-button.tsx
    Co-authored-by: jojikun <110207608+0xjojikun@users.noreply.github.com>
    Signed-off-by: Jonathan Prozzi <jonathanprozzi@gmail.com>

--- apps/portal/app/components/list/save-button.tsx
+++ apps/portal/app/components/list/save-button.tsx
@@ -74,7 +74,7 @@ const SaveButton: React.FC<SaveButtonProps> = ({
       state.status === 'transaction-confirmed' ||
       state.status === 'complete'
     ) {
-      return 'Go to Identity'
+      return 'View identity'
     } else if (state.status === 'error') {
       return 'Retry'
     } else if (chain?.id !== getChainEnvConfig(CURRENT_ENV).chainId) {
