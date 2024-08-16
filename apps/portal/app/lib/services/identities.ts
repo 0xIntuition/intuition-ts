882e8a1554511b1bd5235eacc796d49e8a8037cc
jonathanprozzi <jonathanprozzi@gmail.com>
2024-08-16 09:29:10 -0400

    Wraps the tag functionality in isPending as well

--- apps/portal/app/lib/services/identities.ts
+++ apps/portal/app/lib/services/identities.ts
@@ -21,16 +21,18 @@ export async function getIdentityOrPending(
     if (error instanceof ApiError && error.status === 404) {
       try {
         logger('checking pending status')
-        const pendingIdentity = await fetchWrapper(request, {
-          method: IdentitiesService.getIdentityById,
-          args: { id: 'de2456d4-70fc-41d9-9fda-51624545036e' }, // currently this is params.id (but we'll pass this in)
-        })
-        return { identity: pendingIdentity, isPending: true }
         // const pendingIdentity = await fetchWrapper(request, {
-        //   method: IdentitiesService.getPendingIdentities,
-        //   args: { id }, // not in place yet in BE
+        //   method: IdentitiesService.getIdentityById,
+        //   args: { id: 'de2456d4-70fc-41d9-9fda-51624545036e' }, // currently this is params.id (but we'll pass this in)
         // })
         // return { identity: pendingIdentity, isPending: true }
+        const pendingIdentity = (await fetchWrapper(request, {
+          method: IdentitiesService.getPendingIdentity,
+          args: { identifier: id },
+        })) as unknown as IdentityPresenter
+        logger('id', id)
+        logger('pending identity', pendingIdentity)
+        return { identity: pendingIdentity, isPending: true }
       } catch (pendingError) {
         logger('catching pendingError')
         return { identity: null, isPending: false }
