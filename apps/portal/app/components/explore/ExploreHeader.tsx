ff370fcd1de6ca6f7c1f32e8e6c19767ff1c0d8a
jonathanprozzi <jonathanprozzi@gmail.com>
2024-08-21 19:35:27 -0400

    Sizing changes and removes on mobile

--- apps/portal/app/components/explore/ExploreHeader.tsx
+++ apps/portal/app/components/explore/ExploreHeader.tsx
@@ -23,15 +23,15 @@ const ExploreHeader: React.FC<ExploreHeaderProps> = ({
 }: ExploreHeaderProps) => {
   return (
     <div
-      className={`relative flex w-[973px] h-[250px] justify-center w-full rounded-xl theme-border bg-right bg-no-repeat`}
+      className={`hidden relative md:flex w-full h-52 justify-center w-full rounded-xl theme-border bg-right bg-no-repeat`}
       style={{ backgroundImage: `url(${bgImage})` }}
     >
       <div className="w-full px-6 py-4">
-        <Text variant={TextVariant.heading3} weight={TextWeight.semibold}>
+        <Text variant={TextVariant.heading5} weight={TextWeight.semibold}>
           {title}
         </Text>
         <Text
-          variant={TextVariant.headline}
+          variant={TextVariant.bodyLarge}
           weight={TextWeight.normal}
           className="mt-2.5 md:w-1/3 md:text-secondary-foreground"
         >
@@ -39,7 +39,7 @@ const ExploreHeader: React.FC<ExploreHeaderProps> = ({
         </Text>
       </div>
       <div className="absolute bottom-4 right-6">
-        <Icon name={icon} className="w-12 h-12 text-white" />
+        <Icon name={icon} className="w-8 h-8 text-white" />
       </div>
     </div>
   )
