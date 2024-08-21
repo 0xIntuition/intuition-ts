792a12287820b3f32afe6384d2e1d64da24ed3da
jojikun <110207608+0xjojikun@users.noreply.github.com>
2024-08-21 19:24:44 -0400

    Update apps/portal/app/components/create-claim/create-claim-popovers.tsx
    Co-authored-by: Jonathan Prozzi <jonathanprozzi@gmail.com>
    Signed-off-by: jojikun <110207608+0xjojikun@users.noreply.github.com>

--- apps/portal/app/components/create-claim/create-claim-popovers.tsx
+++ apps/portal/app/components/create-claim/create-claim-popovers.tsx
@@ -80,7 +80,7 @@ export const IdentityPopover: React.FC<IdentityPopoverProps> = ({
               }
             />
           </div>
-          <HoverCard openDelay={150} closeDelay={150}>
+          <HoverCard openDelay={150} closeDelay={100}>
             <HoverCardTrigger className="w-full">
               <IdentityTag
                 size="lg"
