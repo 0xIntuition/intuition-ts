174992591382c907e2eb969d68a35444e6e35703
jonathanprozzi <jonathanprozzi@gmail.com>
2024-08-21 21:08:09 -0400

    Adds second object id to the featured lists mapping

--- apps/portal/app/lib/utils/app.ts
+++ apps/portal/app/lib/utils/app.ts
@@ -122,8 +122,8 @@ export const getSpecialPredicate = (
 export const getFeaturedListObjectIds = (chainEnv: ChainEnv): number[] => {
   const featuredListObjectIds: Record<ChainEnv, number[]> = {
     development: [33, 22],
-    staging: [45],
-    production: [45],
+    staging: [45, 708],
+    production: [45, 708],
   }
   if (!chainEnv) {
     console.error(
