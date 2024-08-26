8c0ea17045c0594bc1154fcacf49c6807df630a2
Vitalsine85 <rob@creative-element.net>
2024-08-26 17:43:07 -0400

    add custom input to follow modal

--- apps/portal/app/components/follow/follow-actions.tsx
+++ apps/portal/app/components/follow/follow-actions.tsx
@@ -1,4 +1,7 @@
+import { useState } from 'react'
+
 import {
+  Input,
   RadioGroup,
   RadioGroupDivider,
   RadioGroupItem,
@@ -25,6 +28,9 @@ export default function FollowActions({
   validationErrors,
   setValidationErrors,
 }: FollowActionsProps) {
+  const [customValue, setCustomValue] = useState('')
+  const [selectedValue, setSelectedValue] = useState(MIN_DEPOSIT)
+
   const radioGroupData = [
     {
       id: MIN_DEPOSIT,
@@ -37,11 +43,42 @@ export default function FollowActions({
 
   const numberOfRadioGroupItems = radioGroupData.length
 
+  const handleValueChange = (value: string) => {
+    setSelectedValue(value)
+    if (value !== 'custom') {
+      setVal(value)
+    } else {
+      setVal(customValue)
+    }
+    setShowErrors(false)
+    setValidationErrors([])
+  }
+
+  const handleCustomInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value
+    setCustomValue(value)
+    if (selectedValue === 'custom') {
+      e.preventDefault()
+      let inputValue = e.target.value
+      if (inputValue.startsWith('.')) {
+        inputValue = `0${inputValue}`
+      }
+      const sanitizedValue = inputValue.replace(/[^0-9.]/g, '')
+      if (sanitizedValue.split('.').length > 2) {
+        return
+      }
+      setVal(sanitizedValue)
+      setShowErrors(false)
+      setValidationErrors([])
+    }
+  }
+
   return (
     <div className="flex flex-col items-center justify-center">
       <RadioGroup
         defaultValue={radioGroupData[0].id}
-        onValueChange={setVal}
+        value={selectedValue}
+        onValueChange={handleValueChange}
         onChange={(e) => {
           e.preventDefault()
           setShowErrors(false)
@@ -61,6 +98,25 @@ export default function FollowActions({
             {index + 1 < numberOfRadioGroupItems && <RadioGroupDivider />}
           </div>
         ))}
+        <RadioGroupDivider />
+        <div key="custom">
+          <RadioGroupItemContainer className="justify-between flex flex-row px-5 py-4">
+            <div className="flex flex-row gap-1 items-center">
+              <RadioGroupItemLabel htmlFor="custom" value="Custom" />
+              <Input
+                id="position"
+                autoComplete="off"
+                type="text"
+                value={customValue}
+                onChange={handleCustomInputChange}
+                min={'0'}
+                placeholder={'Enter a custom amount'}
+                className="p-0 h-auto border-none bg-transparent text-sm w-full"
+              />
+            </div>
+            <RadioGroupItem value="custom" id="custom" />
+          </RadioGroupItemContainer>
+        </div>
       </RadioGroup>
       <div className={`h-2 px-2 ${!showErrors && 'invisible'}`}>
         <ErrorList errors={validationErrors} />
