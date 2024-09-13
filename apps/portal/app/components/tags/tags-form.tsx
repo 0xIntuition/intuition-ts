7a49ae09365adfb8db18254e189bc99bf8f8ca59
jonathanprozzi <jonathanprozzi@gmail.com>
2024-09-12 21:45:11 -0400

    Fixes typo in the header

--- apps/portal/app/components/tags/tags-form.tsx
+++ apps/portal/app/components/tags/tags-form.tsx
@@ -140,7 +140,7 @@ export function TagsForm({
                   <TabsTrigger
                     variant="alternate"
                     value="view"
-                    label="Exisiting Tags"
+                    label="Existing Tags"
                   />
                   {!readOnly && (
                     <TabsTrigger
