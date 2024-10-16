0b6cfb56cbea006d3745e3ce93c40e5a00ed302a
jonathanprozzi <jonathanprozzi@gmail.com>
2024-10-16 14:43:28 -0400

    Checkpoint for the auth fix -- adding other pieces back in step by step

--- apps/template/app/routes/app.tsx
+++ apps/template/app/routes/app.tsx
@@ -1,17 +1,13 @@
-import { useEffect } from 'react'
-
-import PrivyLogout from '@client/privy-logout'
 import logger from '@lib/utils/logger'
 import { invariant } from '@lib/utils/misc'
 import { json, LoaderFunctionArgs } from '@remix-run/node'
-import { Outlet, useLoaderData, useLocation } from '@remix-run/react'
+import { Outlet } from '@remix-run/react'
 import { requireUserWallet } from '@server/auth'
 
 export async function loader({ request }: LoaderFunctionArgs) {
   logger('[Loader] Entering app loader')
 
   const wallet = await requireUserWallet(request)
-  logger('wallet', wallet)
   invariant(wallet, 'Unauthorized')
 
   return json({
@@ -20,17 +16,9 @@ export async function loader({ request }: LoaderFunctionArgs) {
 }
 
 export default function App() {
-  const { wallet } = useLoaderData<{ wallet: string }>()
-  const { pathname } = useLocation()
-
-  useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [pathname])
-
   return (
     <div className="h-screen flex">
       <Outlet />
-      <PrivyLogout wallet={wallet} />
     </div>
   )
 }
