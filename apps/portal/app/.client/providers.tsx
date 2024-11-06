59b4241bc84cbf549002c55ead68540c6505d5b4
jonathanprozzi <jonathanprozzi@gmail.com>
2024-11-06 13:49:35 -0500

    Adds HydrationBoundary support to the providers.tsx in Portal

--- apps/portal/app/.client/providers.tsx
+++ apps/portal/app/.client/providers.tsx
@@ -2,7 +2,11 @@ import { wagmiConfig } from '@lib/utils/wagmi'
 import type { PrivyClientConfig } from '@privy-io/react-auth'
 import { PrivyProvider } from '@privy-io/react-auth'
 import { WagmiProvider } from '@privy-io/wagmi'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import {
+  HydrationBoundary,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -29,9 +33,11 @@ const privyConfig: PrivyClientConfig = {
 export default function Providers({
   privyAppId,
   children,
+  dehydratedState,
 }: {
   privyAppId: string
   children: React.ReactNode
+  dehydratedState?: unknown
 }) {
   return (
     <PrivyProvider
@@ -41,9 +47,11 @@ export default function Providers({
       config={privyConfig}
     >
       <QueryClientProvider client={queryClient}>
-        <WagmiProvider config={wagmiConfig} reconnectOnMount={false}>
-          {children}
-        </WagmiProvider>
+        <HydrationBoundary state={dehydratedState}>
+          <WagmiProvider config={wagmiConfig} reconnectOnMount={false}>
+            {children}
+          </WagmiProvider>
+        </HydrationBoundary>
       </QueryClientProvider>
     </PrivyProvider>
   )
