36bc690804593990d3cf097fa8a5380a4139b588
jonathanprozzi <jonathanprozzi@gmail.com>
2024-12-18 15:02:32 -0500

    Removes unused items in public from remix scaffolding, fixes eslint config mismatch (which resolves @ alias imports), adds in fonts and favicon that we used in Portal

--- apps/launchpad/app/root.tsx
+++ apps/launchpad/app/root.tsx
@@ -1,12 +1,53 @@
 import {
   Links,
   Meta,
+  MetaFunction,
   Outlet,
   Scripts,
   ScrollRestoration,
 } from '@remix-run/react'
 
 import './styles/globals.css'
+
+import { API_URL_LOCAL, configureClient } from '@0xintuition/graphql'
+
+import Providers from '@components/providers'
+
+// Configure GraphQL client at module initialization using the URLs from the package. For now, we should use the local URL for development
+// This can be updated to use the same environment approach that we use in Portal in the future, or leave up to the template user to configure however makes sense for their use case
+configureClient({
+  apiUrl: API_URL_LOCAL,
+})
+
+export const meta: MetaFunction = ({ data }) => {
+  return [
+    { title: data ? 'Intuition Launchpad' : 'Error | Intuition Launchpad' },
+    {
+      name: 'description',
+      content: `Intuition is an ecosystem of technologies composing a universal and permissionless knowledge graph, capable of handling both objective facts and subjective opinions - delivering superior data for intelligences across the spectrum, from human to artificial.`,
+    },
+    {
+      property: 'og:image',
+      content:
+        'https://res.cloudinary.com/dfpwy9nyv/image/upload/f_auto,q_auto/v1/Portal%20Assets/Site%20Metadata/site-og-image',
+    },
+    { property: 'og:site_name', content: 'Intuition Launchpad' },
+    { property: 'og:locale', content: 'en_US' },
+    {
+      name: 'twitter:card',
+      content: 'summary_large_image',
+    },
+    {
+      name: 'twitter:title',
+      content: 'Intuition Explorer',
+    },
+    {
+      name: 'twitter:description',
+      content: 'Bringing trust to trustless systems.',
+    },
+    { name: 'twitter:site', content: '@0xIntuition' },
+  ]
+}
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -27,5 +68,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <Outlet />
+  return (
+    <main className="relative flex min-h-screen w-full flex-col justify-between antialiased">
+      <Providers>
+        <Outlet />
+      </Providers>
+    </main>
+  )
 }
