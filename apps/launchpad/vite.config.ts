5f1b4436ea9f7f5830267518e2342d74eea815fd
jonathanprozzi <jonathanprozzi@gmail.com>
2024-12-18 15:17:10 -0500

    Opts in to React Router v7 features, adds stats fetch to network, cleans up comments, brings in logger

--- apps/launchpad/vite.config.ts
+++ apps/launchpad/vite.config.ts
@@ -4,12 +4,17 @@ import autoprefixer from 'autoprefixer'
 import { flatRoutes } from 'remix-flat-routes'
 import tailwindcss from 'tailwindcss'
 import { defineConfig } from 'vite'
-// import envOnly from 'vite-env-only'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 import { themePreset } from '../../packages/1ui/src/styles'
 
 installGlobals()
+
+declare module '@remix-run/node' {
+  interface Future {
+    v3_singleFetch: true
+  }
+}
 
 export default defineConfig({
   css: {
@@ -18,8 +23,14 @@ export default defineConfig({
     },
   },
   plugins: [
-    // envOnly(),
     remix({
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+        v3_singleFetch: true,
+        v3_lazyRouteDiscovery: true,
+      },
       ignoredRouteFiles: ['**/.*'],
       routes: async (defineRoutes) => {
         return flatRoutes('routes', defineRoutes, {
@@ -47,34 +58,8 @@ export default defineConfig({
       }
     },
   },
+  // add this back in once we bring Privy in -- likely still needed
   // ssr: {
   //   noExternal: ['@privy-io/react-auth', '@privy-io/wagmi'],
   // },
 })
-
-// this is the NEW recommended config -- using portal's for now to keep it aligned but we can gradually update to the new features
-
-// import { vitePlugin as remix } from "@remix-run/dev";
-// import { defineConfig } from "vite";
-// import tsconfigPaths from "vite-tsconfig-paths";
-
-// declare module "@remix-run/node" {
-//   interface Future {
-//     v3_singleFetch: true;
-//   }
-// }
-
-// export default defineConfig({
-//   plugins: [
-//     remix({
-//       future: {
-//         v3_fetcherPersist: true,
-//         v3_relativeSplatPath: true,
-//         v3_throwAbortReason: true,
-//         v3_singleFetch: true,
-//         v3_lazyRouteDiscovery: true,
-//       },
-//     }),
-//     tsconfigPaths(),
-//   ],
-// });
