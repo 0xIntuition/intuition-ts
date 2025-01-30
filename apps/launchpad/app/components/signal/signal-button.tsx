0da64f3ad58117536e2919d1d86c6aef8a3ca6eb
Vital <rob@creative-element.net>
2025-01-29 19:17:02 -0500

    ENG-5664 feat(launchpad,portal,graphql): bring signaling closer to portal state (#1079)
    Apps
    
    - [ ] data populator
    - [x] portal
    - [ ] template
    - [x] launchpad
    
    Packages
    
    - [ ] 1ui
    - [ ] api
    - [x] graphql
    - [ ] protocol
    - [ ] sdk
    
    Tools
    
    - [ ] tools
    
    Working off the previous PR, ports some of Portals staking components
    into Launchpad. Updated the button components in the table rows, as well
    as implemented the math to track the users ticks, or up-votes, and total
    up-votes. This keeps things more in-line with the survey/question
    experience. Users col now shows the number of up-votes on each entry, to
    calculate this we do: `forTVL / minDeposit`. We can add downvotes when
    we introduce staking against.
    
    https://www.loom.com/share/f78f262c98344081a0e8edc5f5c9caee?sid=151eabf6-7c82-4b26-aecc-a7a0bb5a5911
    
    - [x] I hereby declare that I have abided by the rules and regulations
    as outlined in the
    [CONTRIBUTING.md](https://github.com/0xIntuition/intuition-ts/blob/main/CONTRIBUTING.md)

---
+++ apps/launchpad/app/components/signal/signal-button.tsx
@@ -0,0 +1,160 @@
+import { useEffect, useState } from 'react'
+
+import { Button } from '@0xintuition/1ui'
+
+import { stakeModalAtom } from '@lib/state/store'
+import { getChainEnvConfig } from '@lib/utils/environment'
+import { useNavigation } from '@remix-run/react'
+import { CURRENT_ENV } from 'app/consts'
+import {
+  TransactionActionType,
+  TransactionStateType,
+} from 'app/types/transaction'
+import { useSetAtom } from 'jotai'
+import { formatUnits } from 'viem'
+import { useAccount, useSwitchChain } from 'wagmi'
+
+interface SignalButtonProps {
+  val: string
+  mode: string | undefined
+  handleAction: () => void
+  handleClose: () => void
+  dispatch: (action: TransactionActionType) => void
+  state: TransactionStateType
+  min_deposit: string
+  walletBalance: string
+  shares: string
+  setValidationErrors: (errors: string[]) => void
+  setShowErrors: (show: boolean) => void
+  current_share_price: string
+  id?: string
+  claimOrIdentity?: string
+  className?: string
+}
+
+const SignalButton: React.FC<SignalButtonProps> = ({
+  val,
+  mode,
+  handleAction,
+  handleClose,
+  dispatch,
+  state,
+  min_deposit,
+  walletBalance,
+  shares,
+  setValidationErrors,
+  setShowErrors,
+  current_share_price,
+}) => {
+  const { switchChain } = useSwitchChain()
+
+  const handleSwitch = () => {
+    if (switchChain) {
+      switchChain({ chainId: getChainEnvConfig(CURRENT_ENV).chainId })
+    }
+  }
+
+  const { address, chain } = useAccount()
+
+  const formattedCurrentSharePrice = formatUnits(
+    BigInt(current_share_price),
+    18,
+  )
+  const formattedShares = formatUnits(BigInt(shares), 18)
+
+  const getButtonText = () => {
+    if (val === '') {
+      return 'Enter an Amount'
+    } else if (state.status === 'review-transaction') {
+      return 'Confirm'
+    } else if (state.status === 'awaiting') {
+      return 'Continue in Wallet'
+    } else if (state.status === 'transaction-pending') {
+      return 'Pending'
+    } else if (
+      state.status === 'transaction-confirmed' ||
+      state.status === 'complete'
+    ) {
+      return 'Close'
+    } else if (state.status === 'error') {
+      return 'Retry'
+    } else if (chain?.id !== getChainEnvConfig(CURRENT_ENV).chainId) {
+      return 'Switch Network'
+    }
+    return `Review ${mode === 'deposit' ? 'Deposit' : 'Redeem'}`
+  }
+
+  const setStakeModalActive = useSetAtom(stakeModalAtom)
+
+  const navigation = useNavigation()
+  const [navigationStarted, setNavigationStarted] = useState(false)
+
+  useEffect(() => {
+    if (navigation.state !== 'idle') {
+      setNavigationStarted(true)
+    }
+  }, [navigation.state])
+
+  useEffect(() => {
+    if (navigation.state === 'idle' && navigationStarted) {
+      setStakeModalActive({
+        isOpen: false,
+        id: null,
+        vaultId: '0',
+      })
+      setNavigationStarted(false)
+    }
+  }, [navigation.state, navigationStarted])
+
+  return (
+    <Button
+      variant="primary"
+      onClick={(e) => {
+        e.preventDefault()
+        if (
+          state.status === 'complete' ||
+          state.status === 'transaction-confirmed'
+        ) {
+          handleClose()
+        } else if (state.status === 'review-transaction') {
+          dispatch({ type: 'APPROVE_TRANSACTION' })
+          handleAction()
+        } else if (chain?.id !== getChainEnvConfig(CURRENT_ENV).chainId) {
+          handleSwitch()
+        } else if (val !== '') {
+          const errors = []
+          if (mode === 'deposit' && +val < +min_deposit) {
+            errors.push(`Minimum deposit is ${min_deposit} ETH`)
+          }
+          if (
+            mode === 'deposit'
+              ? +val > +walletBalance
+              : +val > +formattedShares * +formattedCurrentSharePrice
+          ) {
+            errors.push('Insufficient funds')
+          }
+
+          if (errors.length > 0) {
+            setValidationErrors(errors)
+            setShowErrors(true)
+          } else {
+            dispatch({ type: 'REVIEW_TRANSACTION' })
+            setValidationErrors([])
+          }
+        }
+      }}
+      disabled={
+        !address ||
+        val === '' ||
+        state.status === 'confirm' ||
+        state.status === 'transaction-pending' ||
+        state.status === 'awaiting'
+      }
+      className="w-40 mx-auto mt-10"
+    >
+      {getButtonText()}
+    </Button>
+  )
+}
+
+export default SignalButton
