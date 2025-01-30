7defd48b64f9e0d5c1043e8c21ab4f4f455cf082
Jonathan Prozzi <jonathanprozzi@gmail.com>
2025-01-30 12:01:54 -0500

    ENG-5682 feat(launchpad): temporarily removes network page (#1086)
    - Temporarily removes the network page until we lock in some of the data
    questions
    - We can decide if we want to merge this in and remove or hold off --
    can quickly add back in once things are locked

--- apps/launchpad/app/components/app-sidebar.tsx
+++ apps/launchpad/app/components/app-sidebar.tsx
@@ -76,12 +76,13 @@ export function AppSidebar({
       href: '/quests/questions',
       isAccent: location.pathname.startsWith('/quests/questions'),
     },
-    {
-      iconName: 'circle-arrow',
-      label: 'Network',
-      href: '/network',
-      isAccent: location.pathname === '/network',
-    },
+    // TODO: Add this back in once we resolve query questions
+    // {
+    //   iconName: 'circle-arrow',
+    //   label: 'Network',
+    //   href: '/network',
+    //   isAccent: location.pathname === '/network',
+    // },
   ]
 
   const footerNavItems: NavItem[] = [
