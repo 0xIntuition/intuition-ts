9dd04b3e5b48af05393151104094c71e937b8524
Vitalsine85 <rob@creative-element.net>
2025-02-04 15:19:38 -0500

    address comments and cleanup

--- apps/launchpad/app/lib/graphql/client.ts
+++ apps/launchpad/app/lib/graphql/client.ts
@@ -1,9 +1,9 @@
 import { GraphQLClient } from 'graphql-request'
 
-// if (process.env.HASURA_POINTS_ENDPOINT === undefined) {
-//   throw new Error('Points API endpoint not defined')
-// }
+if (process.env.HASURA_POINTS_ENDPOINT === undefined) {
+  throw new Error('Points API endpoint not defined')
+}
 
 export const pointsClient = new GraphQLClient(
-  'https://intuition-points-api.onrender.com/v1/graphql',
+  process.env.HASURA_POINTS_ENDPOINT,
 )
