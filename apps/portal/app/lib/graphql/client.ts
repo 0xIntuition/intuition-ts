e63b68d4a56a09a231ba9dfafd95818dbd3097a2
Vitalsine85 <rob@creative-element.net>
2025-02-21 17:56:04 -0500

    reset points endpoint

--- apps/portal/app/lib/graphql/client.ts
+++ apps/portal/app/lib/graphql/client.ts
@@ -1,9 +1,9 @@
 import { GraphQLClient } from 'graphql-request'
 
-if (process.env.HASURA_POINTS_ENDPOINT === undefined) {
+if (process.env.POINTS_GRAPHQL_ENDPOINT === undefined) {
   throw new Error('Points API endpoint not defined')
 }
 
 export const pointsClient = new GraphQLClient(
-  process.env.HASURA_POINTS_ENDPOINT,
+  process.env.POINTS_GRAPHQL_ENDPOINT,
 )
