10af089b2a15bb3b735dd413fab80226e41e6cc3
jojikun <110207608+0xjojikun@users.noreply.github.com>
2025-04-05 04:46:42 -0600

    ENG-6405 feat(launchpad): Add ecosystem external links / references (#1150)
    ## Affected Packages
    
    Apps
    
    - [ ] data populator
    - [ ] portal
    - [ ] template
    - [X] launchpad
    
    Packages
    
    - [ ] 1ui
    - [ ] api
    - [ ] graphql
    - [ ] protocol
    - [ ] sdk
    
    Tools
    
    - [ ] tools
    
    ## Overview
    
    * Add ecosystem link to ecosystem question set
    * Add ecosystem link to ecosystems
    * Add Intuition ecosystems to discover
    
    ## Screen Captures
    
    If applicable, add screenshots or screen captures of your changes.
    
    ## Declaration
    
    - [X] I hereby declare that I have abided by the rules and regulations
    as outlined in the
    [CONTRIBUTING.md](https://github.com/0xIntuition/intuition-ts/blob/main/CONTRIBUTING.md)
    
    ---------
    
    Co-authored-by: Jonathan Prozzi <jonathanprozzi@gmail.com>
    Co-authored-by: Vital <rob@creative-element.net>

--- apps/launchpad/app/lib/utils/constants.ts
+++ apps/launchpad/app/lib/utils/constants.ts
@@ -25,9 +25,10 @@ export const QUESTS = [
 ]
 
 export const WHITELISTED_ADDRESSES = [
-  '0xd99811847e634d33f0dace483c52949bec76300f',
-  '0xbb285b543c96c927fc320fb28524899c2c90806c',
-  '0x0b162525c5dc8c18f771e60fd296913030bfe42c',
-  '0xbd2de08af9470c87c4475117fb912b8f1d588d9c',
-  '0xb95ca3d3144e9d1daff0ee3d35a4488a4a5c9fc5',
+  '0xd99811847e634d33f0dace483c52949bec76300f', // Calum
+  '0xbb285b543c96c927fc320fb28524899c2c90806c', // Calum
+  '0x0b162525c5dc8c18f771e60fd296913030bfe42c', // Tibor
+  '0xbd2de08af9470c87c4475117fb912b8f1d588d9c', // Voto
+  '0xb95ca3d3144e9d1daff0ee3d35a4488a4a5c9fc5', // Vital
+  // '0x81766f4ead4d3b35d1a46fbfaaf0c557ff1a860b', // Fvngabill
 ]
